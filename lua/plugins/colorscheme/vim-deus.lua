2ba036c860b87e892729141ae128a95fb4fc00ca
Wei Tan <tanwei0404@163.com>
2023-07-25 12:42:04 +0800

    colorscheme: Add vim-deus

---
+++ lua/plugins/colorscheme/vim-deus.lua
@@ -0,0 +1,13 @@
+local M = {
+  "ajmwagar/vim-deus",
+}
+
+M.init = function()
+  vim.g.deus_termcolors = 256
+
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "deus")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
