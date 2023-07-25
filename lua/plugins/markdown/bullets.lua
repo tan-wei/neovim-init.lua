66225356f112c879ba4b586af72f8194dffb3758
Wei Tan <tanwei0404@163.com>
2023-07-25 16:15:06 +0800

    plugin: Add bullets.vim

---
+++ lua/plugins/markdown/bullets.lua
@@ -0,0 +1,14 @@
+local M = {
+  "dkarter/bullets.vim",
+}
+
+M.init = function()
+  vim.g.bullets_enabled_file_types = { "markdown" }
+  vim.g.bullets_line_spacing = 1
+  vim.g.bullets_pad_right = 1
+  vim.g.bullets_pad_right = 0
+  vim.g.bullets_checkbox_partials_toggle = 1
+  vim.g.bullets_checkbox_markers = " .oOX" -- or '✗○◐●✓"
+end
+
+return M
