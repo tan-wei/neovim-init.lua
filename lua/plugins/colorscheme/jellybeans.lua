669aa83391f0aea94eaff89c65fad71f3b288daf
Wei Tan <tanwei0404@163.com>
2023-07-25 12:46:53 +0800

    colorscheme: Add jellybeans

---
+++ lua/plugins/colorscheme/jellybeans.lua
@@ -0,0 +1,13 @@
+local M = {
+  "nanotech/jellybeans.vim",
+}
+
+M.init = function()
+  vim.g.jellybeans_use_term_italics = 1
+
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "jellybeans")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
