eb2e1e3379437deef57360f5fbf85315653d096b
Wei Tan <tanwei0404@163.com>
2023-07-25 15:11:27 +0800

    config: Configure for vim-colors-solarized

--- lua/plugins/colorscheme/vim-colors-solarized.lua
+++ lua/plugins/colorscheme/vim-colors-solarized.lua
@@ -3,6 +3,7 @@ local M = {
 }
 
 M.init = function()
+  vim.g.solarized_termcolors = 256
   local available_colorschemes = vim.g.available_colorschemes or {}
   table.insert(available_colorschemes, "solarized")
   vim.g.available_colorschemes = available_colorschemes
