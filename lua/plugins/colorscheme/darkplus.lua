db9342b3e35126f05ad4e094fa9c6081aaac7ee6
Wei Tan <tanwei0404@163.com>
2023-07-25 11:20:19 +0800

    config: Configure for available colorschemes

--- lua/plugins/colorscheme/darkplus.lua
+++ lua/plugins/colorscheme/darkplus.lua
@@ -2,6 +2,10 @@ local M = {
   "lunarvim/darkplus.nvim",
 }
 
-M.init = function() end
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "darkplus")
+  vim.g.available_colorschemes = available_colorschemes
+end
 
 return M
--- lua/plugins/colorscheme/tokyonight.lua
+++ lua/plugins/colorscheme/tokyonight.lua
@@ -5,6 +5,9 @@ local M = {
 M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
   table.insert(available_colorschemes, "tokyonight")
+  table.insert(available_colorschemes, "tokyonight-storm")
+  table.insert(available_colorschemes, "tokyonight-night")
+  table.insert(available_colorschemes, "tokyonight-moon")
   vim.g.available_colorschemes = available_colorschemes
 end
 
