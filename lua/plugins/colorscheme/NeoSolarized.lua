6901d5b0c7af0e9ec23501f88993dad676ca0e30
Wei Tan <tanwei0404@163.com>
2023-07-25 12:21:42 +0800

    colorscheme: Add NeoSolarized

---
+++ lua/plugins/colorscheme/NeoSolarized.lua
@@ -0,0 +1,11 @@
+local M = {
+  "overcache/NeoSolarized",
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "NeoSolarized")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
