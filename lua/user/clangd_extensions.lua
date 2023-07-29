c89af169d3d60a5f51f72a4f7ad96b3be8919b12
Wei Tan <tanwei0404@163.com>
2023-07-29 15:13:59 +0800

    config: Configure for clangd_extensions.nvim

--- lua/user/clangd_extensions.lua
+++ lua/user/clangd_extensions.lua
@@ -1,7 +1,7 @@
 local status_cmp_ok, clangd_extensions = pcall(require, "clangd_extensions")
--- if not status_ok then
---   return
--- end
+if not status_ok then
+  return
+end
 
 clangd_extensions.setup {
   server = {
