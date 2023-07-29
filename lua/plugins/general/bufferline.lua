38c6b6a686e96ac6d78dad00253df749ead9df1d
Wei Tan <tanwei0404@163.com>
2023-07-29 23:21:20 +0800

    config: Configure for bufferline.nvim

--- lua/plugins/general/bufferline.lua
+++ lua/plugins/general/bufferline.lua
@@ -1,7 +1,7 @@
 local M = {
   "akinsho/bufferline.nvim",
   tag = "*",
-  dependencies = "nvim-tree/nvim-web-devicons",
+  dependencies = { "nvim-tree/nvim-web-devicons", "moll/vim-bbye" },
 }
 
 M.init = function() end
