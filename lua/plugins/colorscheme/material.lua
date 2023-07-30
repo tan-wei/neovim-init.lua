6746788850cc5c49dc7839104c6bc4d44a90f499
Wei Tan <tanwei0404@163.com>
2023-07-30 14:23:53 +0800

    colorscheme: Add material.nvim

---
+++ lua/plugins/colorscheme/material.lua
@@ -0,0 +1,12 @@
+local M = {
+  "marko-cerovac/material.nvim",
+}
+
+M.init = function()
+  vim.g.material_style = "deep ocean"
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "material")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
