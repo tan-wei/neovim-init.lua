6877fc58f391618416d6a8c31e002d8ce2e99aad
Wei Tan <tanwei0404@163.com>
2023-07-30 14:07:07 +0800

    colorscheme: Add vim-one

---
+++ lua/plugins/colorscheme/vim-one.lua
@@ -0,0 +1,12 @@
+local M = {
+  "rakr/vim-one",
+}
+
+M.init = function()
+  vim.g.one_allow_italics = 1
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "one")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
