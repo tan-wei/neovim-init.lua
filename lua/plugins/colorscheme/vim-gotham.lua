bee1519447a0e4074d29302cdcbbe5e9a892cf93
Wei Tan <tanwei0404@163.com>
2023-07-30 14:15:48 +0800

    colorscheme: vim-gotham

---
+++ lua/plugins/colorscheme/vim-gotham.lua
@@ -0,0 +1,11 @@
+local M = {
+  "whatyouhide/vim-gotham",
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "gotham256")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
