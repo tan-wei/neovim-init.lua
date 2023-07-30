fc7aa6964e805bf3fb34c10bdb55fd6a83343044
Wei Tan <tanwei0404@163.com>
2023-07-30 14:11:41 +0800

    colorscheme: Add ocean-next

--- lua/plugins/colorscheme/ocean-next.lua
+++ lua/plugins/colorscheme/ocean-next.lua
@@ -3,8 +3,10 @@ local M = {
 }
 
 M.init = function()
+  vim.g.oceanic_next_terminal_bold = 1
+  vim.g.oceanic_next_terminal_italic = 1
   local available_colorschemes = vim.g.available_colorschemes or {}
-  table.insert(available_colorschemes, "nord")
+  table.insert(available_colorschemes, "OceanicNext")
   vim.g.available_colorschemes = available_colorschemes
 end
 
