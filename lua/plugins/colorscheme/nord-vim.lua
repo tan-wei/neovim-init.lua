1b812a214e6e63bd28cf09fc7388158af3680a17
Wei Tan <tanwei0404@163.com>
2023-07-30 14:04:46 +0800

    colorscheme: Add nord-vim

---
+++ lua/plugins/colorscheme/nord-vim.lua
@@ -0,0 +1,11 @@
+local M = {
+  "arcticicestudio/nord-vim",
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "nord")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
