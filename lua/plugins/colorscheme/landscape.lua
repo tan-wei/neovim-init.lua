72e5686a2cda5d96cb241ad0f652444f7a2bf1b2
Wei Tan <tanwei0404@163.com>
2023-07-30 14:18:41 +0800

    colorscheme: landscape.vim

---
+++ lua/plugins/colorscheme/landscape.lua
@@ -0,0 +1,11 @@
+local M = {
+  "itchyny/landscape.vim",
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "landscape")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
