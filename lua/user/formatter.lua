a1176da1a9a0ae88a06f159a64db4a7b6cfd00ef
Wei Tan <tanwei0404@163.com>
2023-07-31 13:43:15 +0800

    config: Configure for formatter.nvim

--- lua/user/formatter.lua
+++ lua/user/formatter.lua
@@ -15,45 +15,15 @@ formmater.setup {
     -- Formatter configurations for filetype "lua" go here
     -- and will be executed in order
     lua = {
-      -- "formatter.filetypes.lua" defines default configurations for the
-      -- "lua" filetype
       require("formatter.filetypes.lua").stylua,
-
-      -- We can also define our own configuration
-      function()
-        -- Supports conditional formatting
-        if util.get_current_buffer_file_name() == "special.lua" then
-          return nil
-        end
-
-        -- Full specification of configurations is down below and in Vim help
-        -- files
-        return {
-          exe = "stylua",
-          args = {
-            "--search-parent-directories",
-            "--stdin-filepath",
-            util.escape_path(util.get_current_buffer_file_path()),
-            "--",
-            "-",
-          },
-          stdin = true,
-        }
-      end,
     },
 
     c = {
-      function()
-        return {
-          exe = "clang-format",
-          args = {
-            "-assume-filename=",
-            vim.fn.shellescape(vim.api.nvim_buf_get_name(0)),
-            '-style="{BasedOnStyle: llvm, IndentWidth: 8}"',
-          },
-          stdin = true,
-        }
-      end,
+      require("formatter.filetypes.c").clangformat,
+    },
+
+    cpp = {
+      require("formatter.filetypes.c").clangformat,
     },
 
     -- Use the special "*" filetype for defining formatter configurations on
