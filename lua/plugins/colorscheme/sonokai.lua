4e2e182e897e4ad35c73e00c4f5bb3ddabf39159
Wei Tan <tanwei0404@163.com>
2023-07-31 11:16:26 +0800

    colorscheme: Add sonokai

---
+++ lua/plugins/colorscheme/sonokai.lua
@@ -0,0 +1,11 @@
+local M = {
+  "sainnhe/sonokai",
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "sonokai")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
