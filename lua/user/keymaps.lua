59acd1ac47a7f3f1934af71d53d29b75d59ec159
Wei Tan <tanwei0404@163.com>
2023-08-02 08:37:57 +0800

    refactor: Add some notes

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -68,6 +68,7 @@ keymap("x", "<A-k>", ":m '<-2<CR>gv=gv", opts)
 -- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 -- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
--- clever-f
+-- clever-f --
+-- Keeping the functionality of ; and , via mappings
 keymap("", ";", "<Plug>(clever-f-repeat-forward)", opts)
 keymap("", ",", "<Plug>(clever-f-repeat-back)", opts)
