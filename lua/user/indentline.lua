dac957521eb38c53cada3896768bf88d41c5967d
Wei Tan <tanwei0404@163.com>
2023-08-03 10:22:23 +0800

    config: Configure for indentline

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -20,7 +20,7 @@ vim.g.indent_blankline_char = "▏"
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 vim.g.indent_blankline_show_first_indent_level = true
 vim.g.indent_blankline_use_treesitter = true
-vim.g.indent_blankline_show_current_context = true
+vim.g.indent_blankline_show_current_context = false
 vim.g.indent_blankline_context_patterns = {
   "class",
   "return",
@@ -66,10 +66,10 @@ vim.opt.listchars:append "space:⋅"
 vim.opt.listchars:append "eol:↴"
 
 indent_blankline.setup {
-  show_end_of_line = true,
+  show_end_of_line = false,
   space_char_blankline = " ",
-  show_current_context = true,
-  show_current_context_start = true,
+  show_current_context = false,
+  show_current_context_start = false,
   char_highlight_list = {
     "IndentBlanklineIndent1",
     "IndentBlanklineIndent2",
