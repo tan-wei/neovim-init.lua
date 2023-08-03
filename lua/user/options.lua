11801f125110096a4eca90c0a6ce61209bba0942
Wei Tan <tanwei0404@163.com>
2023-08-03 12:33:07 +0800

    config: Configure for Neovim-qt

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -41,7 +41,7 @@ local options = {
   linebreak = true, -- companion to wrap, don't split words
   scrolloff = 8, -- minimal number of screen lines to keep above and below the cursor
   sidescrolloff = 8, -- minimal number of screen columns either side of cursor if wrap is `false`
-  guifont = "monospace:h12", -- the font used in graphical neovim applications
+  guifont = "Iosevka Nerd Font Mono:h10", -- the font used in graphical neovim applications
   whichwrap = "bs<>[]hl", -- which "horizontal" keys are allowed to travel to prev/next line
   foldenable = false, -- Default not fold any thing
 }
