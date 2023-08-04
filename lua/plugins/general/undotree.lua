0473b5f560e41297d41277a44a92e7cb0d64a798
Wei Tan <tanwei0404@163.com>
2023-08-04 09:14:49 +0800

    config: Configure for undotree

--- lua/plugins/general/undotree.lua
+++ lua/plugins/general/undotree.lua
@@ -9,7 +9,7 @@ M.init = function()
     local undotree_dir = vim.fn.stdpath "data" .. "/undotree"
     local target_path = vim.fn.expand(undotree_dir)
     if vim.fn.isdirectory(target_path) == 0 or vim.fn.isdirectory(target_path) == false then
-      vim.fn.mkdir(target_path, "p", 0700)
+      vim.fn.mkdir(target_path, "p")
     end
     vim.o.undodir = target_path
     vim.o.undofile = true
