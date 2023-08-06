1497fd604d2e5397afea55b41a77da598689c709
Wei Tan <tanwei0404@163.com>
2023-08-06 09:58:35 +0800

    fix: Fix installation on Windows

--- lua/plugins/general/telescope.lua
+++ lua/plugins/general/telescope.lua
@@ -1,7 +1,7 @@
 local M = {
   "nvim-telescope/telescope.nvim",
   dependencies = {
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build" },
     "nvim-treesitter/nvim-treesitter",
     "nvim-tree/nvim-web-devicons",
     "nvim-lua/plenary.nvim",
