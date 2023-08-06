f24262ea5aa7480c980bf50a9614f975a4b1d9e2
Wei Tan <tanwei0404@163.com>
2023-08-06 10:27:43 +0800

    config: Configure for onedarkpro.nvim

--- lua/plugins/colorscheme/onedarkpro.lua
+++ lua/plugins/colorscheme/onedarkpro.lua
@@ -5,6 +5,7 @@ local M = {
 M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
   table.insert(available_colorschemes, "onedark_dark")
+  table.insert(available_colorschemes, "onedark_vivid")
   vim.g.available_colorschemes = available_colorschemes
 end
 
