202603008e8fb6847690a10ee0a73af5af71e4df
Wei Tan <tanwei0404@163.com>
2023-08-06 11:19:19 +0800

    style: Format code and remove deprecated code

--- lua/plugins/snippet/LuaSnip.lua
+++ lua/plugins/snippet/LuaSnip.lua
@@ -2,16 +2,16 @@ local M = {
   "L3MON4D3/LuaSnip",
   version = "2.*",
   build = function()
-    if vim.loop.os_uname().sysname ~= "Windows_NT" then
-    local job = require "plenary.job"
-    job
-      :new({
-        command = "make install_jsregexp",
-        cwd = vim.fn.stdpath "data" .. "/lazy/LuaSnip",
-      })
-      :sync()
+    if vim.uv.os_uname().sysname ~= "Windows_NT" then
+      local job = require "plenary.job"
+      job
+        :new({
+          command = "make install_jsregexp",
+          cwd = vim.fn.stdpath "data" .. "/lazy/LuaSnip",
+        })
+        :sync()
     end
-  end
+  end,
 }
 
 M.init = function() end
