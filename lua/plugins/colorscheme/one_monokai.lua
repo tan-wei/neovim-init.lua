32854608994fbf06e578d369eb8d42e7dfc3598e
Wei Tan <tanwei0404@163.com>
2023-08-08 11:23:55 +0800

    colorscheme: Add one_monokai.nvim

---
+++ lua/plugins/colorscheme/one_monokai.lua
@@ -0,0 +1,13 @@
+local M = {
+  "cpea2506/one_monokai.nvim",
+}
+
+M.init = function()
+  vim.g.onedark_termcolors = 256
+
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "one_monokai")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -95,7 +95,6 @@ vim.opt.rtp:prepend(install_path)
 --   { "agude/vim-eldar" },
 --   { "pineapplegiant/spaceduck", branch = "main" },
 
---   { "cpea2506/one_monokai.nvim" },
 --   { "shaunsingh/moonlight.nvim" },
 --   {
 --     "kyoz/purify",
