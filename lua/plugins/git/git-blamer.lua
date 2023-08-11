0fa8acd2f41f66705a490907bd8ec4abe8b95c6c
Wei Tan <tanwei0404@163.com>
2023-08-11 10:05:29 +0800

    config: Configure for git-blame

--- lua/plugins/git/git-blamer.lua
+++ lua/plugins/git/git-blamer.lua
@@ -5,6 +5,8 @@ local M = {
 M.init = function()
   vim.g.gitblame_enabled = 1
   vim.g.gitblame_message_template = "<summary> • <date> • <author>"
+  vim.g.gitblame_ignored_filetypes = { "markdown" }
+  vim.g.gitblame_delay = 1000
 end
 
 return M
