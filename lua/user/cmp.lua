508718ae97719c746265104f1f56dcaa6616a5a6
Wei Tan <tanwei0404@163.com>
2023-08-15 13:58:09 +0800

    plugin: Just disable cmp-treesitter, bucause it is not necessary to enbale on all filetypes

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -118,7 +118,7 @@ cmp.setup {
   sources = {
     { name = "nvim_lsp" },
     { name = "nvim_lsp_signature_help" },
-    { name = "treesitter" },
+    -- { name = "treesitter" },
     { name = "luasnip" },
     { name = "buffer" },
     { name = "path" },
