73e18b97c5156d4f6e6a2e26012cbdd57f24bc41
Wei Tan <tanwei0404@163.com>
2023-08-15 22:28:35 +0800

    treesitter: Add cpp parser installation

--- lua/user/nvim-treesitter.lua
+++ lua/user/nvim-treesitter.lua
@@ -6,7 +6,7 @@ end
 local treesitter = require "nvim-treesitter"
 
 nvim_treesitter_configs.setup {
-  ensure_installed = { "c", "vim", "rust", "ruby", "lua", "markdown", "markdown_inline", "bash", "python" }, -- put the language you want in this array
+  ensure_installed = { "c", "cpp", "vim", "rust", "ruby", "lua", "markdown", "markdown_inline", "bash", "python" }, -- put the language you want in this array
   -- ensure_installed = "all", -- one of "all" or a list of languages
   ignore_install = { "" }, -- List of parsers to ignore installing
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
