8058217c0dd98e872002c86451bfad49f2f62abc
Wei Tan <tanwei0404@163.com>
2023-08-15 12:10:43 +0800

    config: Configure for nvim-treesitter

--- lua/user/nvim-treesitter.lua
+++ lua/user/nvim-treesitter.lua
@@ -1,30 +1,30 @@
-local M = {}
-function M.config()
-  local treesitter = require "nvim-treesitter"
-  local configs = require "nvim-treesitter.configs"
-
-  configs.setup {
-    ensure_installed = { "c", "vim", "rust", "ruby", "lua", "markdown", "markdown_inline", "bash", "python" }, -- put the language you want in this array
-    -- ensure_installed = "all", -- one of "all" or a list of languages
-    ignore_install = { "" }, -- List of parsers to ignore installing
-    sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-
-    highlight = {
-      enable = true, -- false will disable the whole extension
-      disable = { "css" }, -- list of language that will be disabled
-    },
-
-    autopairs = {
-      enable = true,
-    },
-
-    indent = { enable = true, disable = { "python", "css" } },
-
-    context_commentstring = {
-      enable = true,
-      enable_autocmd = false,
-    },
-  }
+local status_ok, nvim_treesitter_configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+  return
 end
 
-return M
+local treesitter = require "nvim-treesitter"
+
+nvim_treesitter_configs.setup {
+  ensure_installed = { "c", "vim", "rust", "ruby", "lua", "markdown", "markdown_inline", "bash", "python" }, -- put the language you want in this array
+  -- ensure_installed = "all", -- one of "all" or a list of languages
+  ignore_install = { "" }, -- List of parsers to ignore installing
+  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+
+  highlight = {
+    enable = true, -- false will disable the whole extension
+    disable = { "css" }, -- list of language that will be disabled
+  },
+  auto_install = true,
+
+  autopairs = {
+    enable = true,
+  },
+
+  indent = { enable = true, disable = { "python", "css" } },
+
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+  },
+}
