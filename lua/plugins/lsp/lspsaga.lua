e1c98fb751d9d07aa26d123dd321284609db02f7
Wei Tan <tanwei0404@163.com>
2023-08-16 17:30:48 +0800

    plugin: Add lspsaga.nvim

---
+++ lua/plugins/lsp/lspsaga.lua
@@ -0,0 +1,11 @@
+local M = {
+  "nvimdev/lspsaga.nvim",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+}
+
+M.init = function() end
+
+return M
