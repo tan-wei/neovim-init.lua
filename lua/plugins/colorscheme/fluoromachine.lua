abce66c3e4df85a1ce5132befb3d824e5adce180
Wei Tan <tanwei0404@163.com>
2023-08-19 19:12:01 +0800

    colorscheme: Add fluoromachine.nvim

---
+++ lua/plugins/colorscheme/fluoromachine.lua
@@ -0,0 +1,11 @@
+local M = {
+  "maxmx03/fluoromachine.nvim",
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "fluoromachine")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
