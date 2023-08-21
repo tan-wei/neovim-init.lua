065c9a3b46717018c1723bf6194a4c1e7500cbd8
Wei Tan <tanwei0404@163.com>
2023-08-21 22:05:39 +0800

    colorscheme: Now citruzest.nvim works well

--- lua/plugins/colorscheme/citruszest.lua
+++ lua/plugins/colorscheme/citruszest.lua
@@ -4,7 +4,7 @@ local M = {
 
 M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
-  -- table.insert(available_colorschemes, "citruszest") -- buggy
+  table.insert(available_colorschemes, "citruszest")
   vim.g.available_colorschemes = available_colorschemes
 end
 
