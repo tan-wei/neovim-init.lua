8c5b5e38490e8d2485809893e6b10482c879d524
Wei Tan <tanwei0404@163.com>
2023-08-21 17:20:42 +0800

    colorscheme: Add citruszest.nvim

---
+++ lua/plugins/colorscheme/citruszest.lua
@@ -0,0 +1,11 @@
+local M = {
+  "zootedb0t/citruszest.nvim",
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  -- table.insert(available_colorschemes, "citruszest") -- buggy
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
