cb0d85309717a472c2516a6440f0018573aead00
Wei Tan <tanwei0404@163.com>
2023-08-21 14:27:22 +0800

    colorscheme: Add nvim-tundra

---
+++ lua/plugins/colorscheme/nvim-tundra.lua
@@ -0,0 +1,11 @@
+local M = {
+  "sam4llis/nvim-tundra",
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "tundra")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
