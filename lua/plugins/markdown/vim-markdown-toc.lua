c8ddf0036e6f3ff6f0c5ee9d4d3156a716ec712d
Wei Tan <tanwei0404@163.com>
2023-08-22 10:57:52 +0800

    config: Configure for vim-markdown-toc

--- lua/plugins/markdown/vim-markdown-toc.lua
+++ lua/plugins/markdown/vim-markdown-toc.lua
@@ -3,9 +3,10 @@ local M = {
 }
 
 M.init = function()
-  vim.g.vmt_auto_update_on_save = 0
+  vim.g.vmt_auto_update_on_save = 1
   vim.g.vmt_dont_insert_fence = 0
   vim.g.vmt_cycle_list_item_markers = 1
+  vim.g.vmt_list_item_chars = { "*", "-", "+" }
 end
 
 return M
