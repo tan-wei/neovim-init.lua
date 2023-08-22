04a6fb7cd5f86ec3de5146c045624da5ec7b5cf3
Wei Tan <tanwei0404@163.com>
2023-08-22 15:48:23 +0800

    colorscheme: Disable nvim-tundra due to its bug

--- lua/plugins/colorscheme/nvim-tundra.lua
+++ lua/plugins/colorscheme/nvim-tundra.lua
@@ -4,7 +4,7 @@ local M = {
 
 M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
-  table.insert(available_colorschemes, "tundra")
+  -- table.insert(available_colorschemes, "tundra") -- buggy
   vim.g.available_colorschemes = available_colorschemes
 end
 
