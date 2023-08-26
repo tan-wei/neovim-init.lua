8fff5a7b0657f6c60e003dda7de4d6a4b6f75498
Wei Tan <tanwei0404@163.com>
2023-08-26 22:07:03 +0800

    colorscheme: Add midnight.nvim

---
+++ lua/plugins/colorscheme/midnight.lua
@@ -0,0 +1,11 @@
+local M = {
+  "dasupradyumna/midnight.nvim",
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "midnight")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
