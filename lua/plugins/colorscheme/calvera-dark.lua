070e903e242ed165c5876697d90325040d712fe3
Wei Tan <tanwei0404@163.com>
2023-08-26 22:16:53 +0800

    colorscheme: Add calvera-dark.nvim

---
+++ lua/plugins/colorscheme/calvera-dark.lua
@@ -0,0 +1,15 @@
+local M = {
+  "hoprr/calvera-dark.nvim",
+}
+
+M.init = function()
+  vim.g.calvera_italic_keywords = false
+  vim.g.calvera_borders = true
+  vim.g.calvera_contrast = true
+  vim.g.calvera_hide_eob = true
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "calvera")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
