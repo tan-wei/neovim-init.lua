797986ba0c04e2d5a82fa1151c264cd77dd26eb7
Wei Tan <tanwei0404@163.com>
2023-08-27 18:31:34 +0800

    colorscheme: Add apprentice.nvim

---
+++ lua/plugins/colorscheme/apprentice.lua
@@ -0,0 +1,11 @@
+local M = {
+  "adisen99/apprentice.nvim",
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "apprentice")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
