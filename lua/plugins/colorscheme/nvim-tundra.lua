e9f98b77a9f6371d88453da7f043052845321af5
Wei Tan <tanwei0404@163.com>
2023-08-27 18:14:40 +0800

    colorscheme: Now nvim-tundra works well

--- lua/plugins/colorscheme/nvim-tundra.lua
+++ lua/plugins/colorscheme/nvim-tundra.lua
@@ -4,7 +4,7 @@ local M = {
 
 M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
-  -- table.insert(available_colorschemes, "tundra") -- buggy
+  table.insert(available_colorschemes, "tundra")
   vim.g.available_colorschemes = available_colorschemes
 end
 
