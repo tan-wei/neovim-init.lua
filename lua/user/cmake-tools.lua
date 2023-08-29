b2ad1e278a40cabf0f0a18f1d906f972adf3dc19
Wei Tan <tanwei0404@163.com>
2023-08-29 16:04:51 +0800

    config: cmake-tools has update setup function

--- lua/user/cmake-tools.lua
+++ lua/user/cmake-tools.lua
@@ -6,7 +6,7 @@ end
 cmake_tools.setup {
   cmake_command = "cmake", -- this is used to specify cmake command path
   cmake_regenerate_on_save = true, -- auto generate when save CMakeLists.txt
-  cmake_generate_options = { "-G Ninja", "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" }, -- this will be passed when invoke `CMakeGenerate`
+  cmake_generate_options = { "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" }, -- this will be passed when invoke `CMakeGenerate`
   cmake_build_options = {}, -- this will be passed when invoke `CMakeBuild`
   cmake_build_directory = "build", -- this is used to specify generate directory for cmake
   cmake_build_directory_prefix = "", -- when cmake_build_directory is set to "", this option will be activated
@@ -25,27 +25,45 @@ cmake_tools.setup {
     runInTerminal = true,
     console = "integratedTerminal",
   },
-  cmake_always_use_terminal = false, -- if true, use terminal for generate, build, clean, install, run, etc, except for debug, else only use terminal for run, use quickfix for others
-  cmake_quickfix_opts = { -- quickfix settings for cmake, quickfix will be used when `cmake_always_use_terminal` is false
-    show = "always", -- "always", "only_on_error"
-    position = "belowright", -- "bottom", "top"
-    size = 10,
+  cmake_executor = { -- executor to use
+    name = "quickfix", -- name of the executor
+    opts = {}, -- the options the executor will get, possible values depend on the executor type. See `default_opts` for possible values.
+    default_opts = { -- a list of default and possible values for executors
+      quickfix = {
+        show = "always", -- "always", "only_on_error"
+        position = "belowright", -- "bottom", "top"
+        size = 10,
+      },
+      overseer = {
+        new_task_opts = {}, -- options to pass into the `overseer.new_task` command
+        on_new_task = function(task) end, -- a function that gets overseer.Task when it is created, before calling `task:start`
+      },
+      terminal = {}, -- terminal executor uses the values in cmake_terminal
+    },
   },
-  cmake_terminal_opts = { -- terminal settings for cmake, terminal will be used for run when `cmake_always_use_terminal` is false or true, will be used for all tasks except for debug when `cmake_always_use_terminal` is true
-    name = "Main Terminal",
-    prefix_name = "[CMakeTools]: ", -- This must be included and must be unique, otherwise the terminals will not work. Do not use a simple spacebar " ", or any generic name
-    split_direction = "horizontal", -- "horizontal", "vertical"
-    split_size = 11,
+  cmake_terminal = {
+    name = "terminal",
+    opts = {
+      name = "Main Terminal",
+      prefix_name = "[CMakeTools]: ", -- This must be included and must be unique, otherwise the terminals will not work. Do not use a simple spacebar " ", or any generic name
+      split_direction = "horizontal", -- "horizontal", "vertical"
+      split_size = 11,
 
-    -- Window handling
-    single_terminal_per_instance = true, -- Single viewport, multiple windows
-    single_terminal_per_tab = true, -- Single viewport per tab
-    keep_terminal_static_location = true, -- Static location of the viewport if avialable
+      -- Window handling
+      single_terminal_per_instance = true, -- Single viewport, multiple windows
+      single_terminal_per_tab = true, -- Single viewport per tab
+      keep_terminal_static_location = true, -- Static location of the viewport if avialable
 
-    -- Running Tasks
-    start_insert_in_launch_task = false, -- If you want to enter terminal with :startinsert upon using :CMakeRun
-    start_insert_in_other_tasks = false, -- If you want to enter terminal with :startinsert upon launching all other cmake tasks in the terminal. Generally set as false
-    focus_on_main_terminal = false, -- Focus on cmake terminal when cmake task is launched. Only used if cmake_always_use_terminal is true.
-    focus_on_launch_terminal = false, -- Focus on cmake launch terminal when executable target in launched.
+      -- Running Tasks
+      start_insert_in_launch_task = false, -- If you want to enter terminal with :startinsert upon using :CMakeRun
+      start_insert_in_other_tasks = false, -- If you want to enter terminal with :startinsert upon launching all other cmake tasks in the terminal. Generally set as false
+      focus_on_main_terminal = false, -- Focus on cmake terminal when cmake task is launched. Only used if executor is terminal.
+      focus_on_launch_terminal = false, -- Focus on cmake launch terminal when executable target in launched.
+    },
+  },
+  cmake_notifications = {
+    enabled = true, -- show cmake execution progress in nvim-notify
+    spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }, -- icons used for progress display
+    refresh_rate_ms = 100, -- how often to iterate icons
   },
 }
