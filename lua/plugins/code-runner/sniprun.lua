82d0d49635e52e51e1d306ed4cb810e1328ac3e8
Wei Tan <tanwei0404@163.com>
2023-08-31 12:24:45 +0800

    plugin: Disable sniprun on Windows

--- lua/plugins/code-runner/sniprun.lua
+++ lua/plugins/code-runner/sniprun.lua
@@ -1,6 +1,13 @@
 local M = {
   "michaelb/sniprun",
   build = "sh ./install.sh",
+  enabled = function()
+    if vim.uv.os_uname().sysname ~= "Windows_NT" then
+      return true
+    else
+      return false
+    end
+  end,
 }
 
 M.init = function() end
