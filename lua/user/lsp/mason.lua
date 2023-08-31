1ec734840135c8978c3cf6504ed2b33b9541e940
Wei Tan <tanwei0404@163.com>
2023-08-31 13:22:35 +0800

    lsp: Use rust-tools.nvim to setup rust-analyzer

--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -10,7 +10,7 @@ local servers = {
   "clangd",
   -- "ccls",
   "ltex",
-  "rust_analyzer",
+  -- "rust_analyzer", -- rust-tools.nvim automatically sets up nvim-lspconfig for rust_analyzer
   "marksman",
   "cmake",
 }
