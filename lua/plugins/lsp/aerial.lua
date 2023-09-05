323a9d908c76978f9d492670b0e649cf03f7d07e
Wei Tan <tanwei0404@163.com>
2023-09-05 11:13:36 +0800

    plugin: Add aerial.nvim

---
+++ lua/plugins/lsp/aerial.lua
@@ -0,0 +1,11 @@
+local M = {
+  "stevearc/aerial.nvim",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+}
+
+M.init = function() end
+
+return M
