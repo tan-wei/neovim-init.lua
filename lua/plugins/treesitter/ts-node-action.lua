88f1dfbbc988443ff013f2ad506250511678e34a
Wei Tan <tanwei0404@163.com>
2023-09-05 14:42:44 +0800

    plugin: Add ts-node-action

---
+++ lua/plugins/treesitter/ts-node-action.lua
@@ -0,0 +1,12 @@
+local M = {
+  "ckolkey/ts-node-action",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  },
+}
+
+-- TODO: Configure for ts-node-action
+
+M.init = function() end
+
+return M
