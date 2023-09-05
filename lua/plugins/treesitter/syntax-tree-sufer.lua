e0196020cc5fba9f531c2f5940a5de2c91d4b606
Wei Tan <tanwei0404@163.com>
2023-09-05 18:14:03 +0800

    plugin: Add syntax-tree-surfer

---
+++ lua/plugins/treesitter/syntax-tree-sufer.lua
@@ -0,0 +1,12 @@
+local M = {
+  "ziontee113/syntax-tree-surfer",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  },
+}
+
+-- TODO: Configure for syntax-tree-surfer
+
+M.init = function() end
+
+return M
