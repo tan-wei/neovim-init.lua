459791df9af83b8a681e96b48e36b642faeb5d39
Wei Tan <tanwei0404@163.com>
2023-09-18 08:38:42 +0800

    plugin: Add lsp-lens.nvim

---
+++ lua/plugins/lsp/lsp-lens.lua
@@ -0,0 +1,7 @@
+local M = {
+  "VidocqH/lsp-lens.nvim",
+}
+
+M.init = function() end
+
+return M
---
+++ lua/user/lsp/lsp-lens.lua
@@ -0,0 +1,17 @@
+local status_ok, lsp_lens = pcall(require, "lsp-lens")
+if not status_ok then
+  return
+end
+
+lsp_lens.setup {
+  enable = true,
+  include_declaration = false, -- Reference include declaration
+  sections = { -- Enable / Disable specific request
+    definition = false,
+    references = true,
+    implements = true,
+  },
+  ignore_filetype = {
+    "prisma",
+  },
+}
