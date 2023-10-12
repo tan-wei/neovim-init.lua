4fcaefb6136362556a8deff57cd1ba7ca4f126b6
Wei Tan <tanwei0404@163.com>
2023-10-12 10:30:52 +0800

    colorscheme: Add flexoki

---
+++ lua/plugins/colorscheme/flexoki.lua
@@ -0,0 +1,13 @@
+local M = {
+  "stevedylandev/flexoki-nvim",
+  name = "flexoki",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "flexoki")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
