ba8200c319dec01c25e58d479b45403587ab25c7
Wei Tan <tanwei0404@163.com>
2023-10-13 08:33:33 +0800

    plugin: Add headlines.nvim

---
+++ lua/plugins/markdown/headlines.lua
@@ -0,0 +1,10 @@
+local M = {
+  "lukas-reineke/headlines.nvim",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  },
+}
+
+M.init = function() end
+
+return M
---
+++ lua/user/headlines.lua
@@ -0,0 +1,10 @@
+local status_ok, headlines = pcall(require, "headlines")
+if not status_ok then
+  return
+end
+
+headlines.setup {
+  markdown = {
+    headline_highlights = false,
+  },
+}
