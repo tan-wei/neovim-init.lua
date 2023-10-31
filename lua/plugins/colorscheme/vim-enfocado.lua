581c6af5aed895a4a04bd6969d4d449df0532f1c
Wei Tan <tanwei0404@163.com>
2023-10-31 16:54:21 +0800

    colorscheme: Configure for vim-enfocado

--- lua/plugins/colorscheme/vim-enfocado.lua
+++ lua/plugins/colorscheme/vim-enfocado.lua
@@ -5,6 +5,7 @@ local M = {
 
 M.init = function()
   vim.g.enfocado_style = "neon"
+  vim.g.enfocado_plugins = { "all" }
   local available_colorschemes = vim.g.available_colorschemes or {}
   table.insert(available_colorschemes, "enfocado")
   vim.g.available_colorschemes = available_colorschemes
