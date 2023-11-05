d72aa5e578ca4fcd1f78bc2a176c129875fa9fe7
Wei Tan <tanwei0404@163.com>
2023-11-05 23:09:12 +0800

    config: Add proto and vimdoc treesitters

--- lua/user/nvim-treesitter.lua
+++ lua/user/nvim-treesitter.lua
@@ -26,6 +26,8 @@ nvim_treesitter_configs.setup {
     "dot",
     "capnp",
     "tsv",
+    "proto",
+    "vimdoc",
   }, -- put the language you want in this array
   -- ensure_installed = "all", -- one of "all" or a list of languages
   ignore_install = { "" }, -- List of parsers to ignore installing
