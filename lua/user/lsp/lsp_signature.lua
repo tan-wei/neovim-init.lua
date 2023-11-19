990ea57967674bcec68fdbd9180ec1891ba93272
Wei Tan <tanwei0404@163.com>
2023-11-19 18:15:27 +0800

    config: Enable lsp_signature's hint

--- lua/user/lsp/lsp_signature.lua
+++ lua/user/lsp/lsp_signature.lua
@@ -6,7 +6,6 @@ end
 lsp_signature.setup {
   log_path = vim.fn.stdpath "cache" .. "/lsp_signature.log",
   debug = true,
-  hint_enable = false,
   handler_opts = { border = "rounded" },
   max_width = 80,
   noice = false, -- TODO: Will cause error, and the author consider to deprecate this option
