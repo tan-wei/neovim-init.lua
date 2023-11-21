80975ab099308c82f272b810b6b999b36d7032ea
Wei Tan <tanwei0404@163.com>
2023-11-21 09:45:24 +0800

    plugin: Add oil.nvim

---
+++ lua/plugins/filesystem/oil.lua
@@ -0,0 +1,12 @@
+local M = {
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+}
+
+M.init = function()
+  -- disable netrw at the very start of our init.lua
+  vim.g.loaded_netrw = 1
+  vim.g.loaded_netrwPlugin = 1
+end
+
+return M
