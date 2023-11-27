704e9682d9d38a51289ae701d06ae90ae67cebd9
Wei Tan <tanwei0404@163.com>
2023-11-27 12:36:50 +0800

    config: Remove vim.o.guifont

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -39,7 +39,6 @@ local options = {
   linebreak = true, -- companion to wrap, don't split words
   scrolloff = 8, -- minimal number of screen lines to keep above and below the cursor
   sidescrolloff = 8, -- minimal number of screen columns either side of cursor if wrap is `false`
-  guifont = "Agave Nerd Font:h8", -- the font used in graphical neovim applications
   whichwrap = "bs<>[]hl", -- which "horizontal" keys are allowed to travel to prev/next line
   foldcolumn = "1", -- The width of a colum on the side of the window to indicate folds
   foldlevel = 99, -- The higher the more folder regions are open
