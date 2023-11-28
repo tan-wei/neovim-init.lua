0f5134c7a68d64803bf379742e22cc97f2a060aa
Wei Tan <tanwei0404@163.com>
2023-11-28 12:22:38 +0800

    config: Configure for bufferline.nvim

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -27,6 +27,9 @@ bufferline.setup {
     diagnostics = "nvim_lsp",
     diagnostics_update_in_insert = true,
     diagnostics_indicator = function(count, level, diagnostics_dict, context)
+      if context.buffer:current() then
+        return ""
+      end
       local icon = level:match "error" and " " or " "
       return " " .. icon .. count
     end,
