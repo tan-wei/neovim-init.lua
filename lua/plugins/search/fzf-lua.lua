caee0800ad3f648c2ec9e24614d7cb6652f4aac6
Wei Tan <tanwei0404@163.com>
2023-12-04 13:51:44 +0800

    config: Fix fzf installation on Windows

--- lua/plugins/search/fzf-lua.lua
+++ lua/plugins/search/fzf-lua.lua
@@ -1,7 +1,23 @@
 local M = {
   "ibhagwan/fzf-lua",
   dependencies = {
-    { "junegunn/fzf", build = "./install --bin" },
+    {
+      "junegunn/fzf",
+      build = function()
+        if vim.uv.os_uname().sysname == "Windows_NT" then
+          local job = require "plenary.job"
+          job
+            :new({
+              command = "powershell",
+              args = { "-File", "./install.ps1", "--bin" },
+              cwd = vim.fn.stdpath "data" .. "/lazy/fzf",
+            })
+            :sync()
+        else
+          return "./install --bin"
+        end
+      end,
+    },
     "nvim-tree/nvim-web-devicons",
   },
 }
