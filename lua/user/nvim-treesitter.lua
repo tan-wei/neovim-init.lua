67d980931cd146a8f71038beeb31b3727cee47de
Wei Tanemai <tanwei0404@163.com>
2023-12-09 15:43:00 +0800

    config: Add some treesitters

--- lua/user/nvim-treesitter.lua
+++ lua/user/nvim-treesitter.lua
@@ -32,6 +32,11 @@ nvim_treesitter_configs.setup {
     "xml",
     "make",
     "go",
+    "gitcommit",
+    "gitignore",
+    "git_config",
+    "git_rebase",
+    "gitattributes",
   }, -- put the language you want in this array
   -- ensure_installed = "all", -- one of "all" or a list of languages
   ignore_install = { "" }, -- List of parsers to ignore installing
