72f29a957520f201fbf38720fe83c23cfa76215f
Wei Tan <tanwei0404@163.com>
2023-12-10 14:22:43 +0800

    refactor: Make Comment.nvim and nvim-ts-context-commentstring lazy load

--- lua/plugins/general/Comment.lua
+++ lua/plugins/general/Comment.lua
@@ -1,5 +1,14 @@
 local M = {
   "numToStr/Comment.nvim",
+  dependencies = {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+  },
 }
+
+M.config = function()
+  require("Comment").setup {
+    pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+  }
+end
 
 return M
--- lua/plugins/treesitter/nvim-ts-context-commentstring.lua
+++ lua/plugins/treesitter/nvim-ts-context-commentstring.lua
@@ -5,4 +5,14 @@ local M = {
   },
 }
 
+M.init = function()
+  vim.g.skip_ts_context_commentstring_module = true
+end
+
+M.config = function()
+  require("ts_context_commentstring").setup {
+    enable_autocmd = false,
+  }
+end
+
 return M
