713201ff1458bc8e7f16034a62c51d87e90ebdf9
Wei Tan <tanwei0404@163.com>
2023-12-10 11:13:02 +0800

    refactor: Make vgit.nvim lazy load

--- lua/plugins/git/vgit.lua
+++ lua/plugins/git/vgit.lua
@@ -2,6 +2,20 @@ local M = {
   "tanvirtin/vgit.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  cmd = { "VGit" },
+}
+
+-- TODO: Keybindings should be configured with which-key
+M.opts = {
+  settings = {
+    live_blame = {
+      enabled = false,
+    },
+    live_gutter = {
+      enabled = false,
+    },
   },
 }
 
