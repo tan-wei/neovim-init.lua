d6d603039392d01c1011e0fa006e8157c0b0123b
Wei Tan <tanwei0404@163.com>
2023-12-10 13:13:47 +0800

    refactor: Make scope.nvim lazy load

--- lua/plugins/general/scope.lua
+++ lua/plugins/general/scope.lua
@@ -1,5 +1,14 @@
 local M = {
   "tiagovla/scope.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+  },
+  cmd = { "ScopeLoadState", "ScopeSaveState" },
 }
+
+M.config = function()
+  require("scope").setup()
+  require("telescope").load_extension "scope"
+end
 
 return M
