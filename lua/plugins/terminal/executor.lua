f261b396dc93e1bbe6383b7b140e7df53c3393c6
Wei Tan <tanwei0404@163.com>
2023-12-10 15:59:08 +0800

    refactor: Make executor.nvim lazy load

--- lua/plugins/terminal/executor.lua
+++ lua/plugins/terminal/executor.lua
@@ -3,6 +3,26 @@ local M = {
   dependencies = {
     "MunifTanjim/nui.nvim",
   },
+  cmd = {
+    "ExecutorRun",
+    "ExecutorSetCommand",
+    "ExecutorShowDetail",
+    "ExecutorHideDetail",
+    "ExecutorToggleDetail",
+    "ExecutorSwapToSplit",
+    "ExecutorSwapToPopup",
+    "ExecutorShowPresets",
+    "ExecutorShowHistory",
+    "ExecutorReset",
+    "ExecutorOneOff",
+  },
+}
+
+M.opts = {
+  use_split = false,
+  output_filter = function(command, lines)
+    return lines
+  end,
 }
 
 return M
