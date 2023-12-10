1b9399c43324836810e41ad19f12c9155db4ba24
Wei Tan <tanwei0404@163.com>
2023-12-10 13:02:02 +0800

    refactor: Make pretty-fold.nvim lazy load

--- lua/plugins/indent/pretty-fold.lua
+++ lua/plugins/indent/pretty-fold.lua
@@ -1,5 +1,9 @@
 local M = {
   "anuvyklack/pretty-fold.nvim",
+  event = "VeryLazy",
 }
+
+-- TODO: This plugin should write more configurations
+M.config = true
 
 return M
--- lua/user/pretty-fold.lua
+++
@@ -1,7 +0,0 @@
-local status_ok, pretty_fold = pcall(require, "prtty-fold")
-if not status_ok then
-  return
-end
-
--- TODO: Configure for different filetype
-pretty_fold.setup()
