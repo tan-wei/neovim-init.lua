cfd9db5102b1c866ad0ac3717a3c66c1d6c4f898
Wei Tan <tanwei0404@163.com>
2023-12-10 16:14:29 +0800

    refactor: Make cmake-tools.nvim lazy load

--- lua/plugins/c++/cmake-tools.lua
+++ lua/plugins/c++/cmake-tools.lua
@@ -3,6 +3,55 @@ local M = {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
+  ft = { "cmake", "c", "cpp" },
+  cmd = {
+    "CMakeGenerate",
+    "CMakeBuild",
+    "CMakeRun",
+    "CMakeDebug",
+    "CMakeLaunchArgs",
+    "CMakeSelectBuildType",
+    "CMakeSelectBuildTarget",
+    "CMakeSelectLaunchTarget",
+    "CMakeSelectKit",
+    "CMakeSelectConfigurePreset",
+    "CMakeSelectBuildPreset",
+    "CMakeSelectCwd",
+    "CMakeSelectBuildDir",
+    "CMakeOpen",
+    "CMakeClose",
+    "CMakeInstall",
+    "CMakeClean",
+    "CMakeStop",
+    "CMakeQuickBuild",
+    "CMakeQuickRun",
+    "CMakeQuickDebug",
+    "CMakeShowTargetFiles",
+    "CMakeSettings",
+    "CMakeTargetSettings",
+  },
+}
+
+M.opts = {
+  cmake_build_directory = "build",
+  cmake_soft_link_compile_commands = false,
+  cmake_compile_commands_from_lsp = true,
+  cmake_executor = {
+    name = "quickfix",
+    opts = {},
+    default_opts = {
+      quickfix = {
+        show = "only_on_error",
+        position = "belowright",
+        size = 10,
+      },
+      overseer = {
+        new_task_opts = {},
+        on_new_task = function(task) end,
+      },
+      terminal = {},
+    },
+  },
 }
 
 return M
