04bb019446267e241275b8c251d9ea9e8a9cd8f1
Wei Tan <tanwei0404@163.com>
2023-12-10 10:34:29 +0800

    refactor: Make numb.nvim  lazy load

--- lua/plugins/general/numb.lua
+++ lua/plugins/general/numb.lua
@@ -1,7 +1,14 @@
 local M = {
   "nacro90/numb.nvim",
+  event = { "CmdlineChanged" },
 }
 
-M.config = function() end
+M.opts = {
+  show_numbers = true,
+  show_cursorline = true,
+  hide_relativenumbers = true,
+  number_only = false,
+  centered_peeking = true,
+}
 
 return M
