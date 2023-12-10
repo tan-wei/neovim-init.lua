bd59ae7e003ca927de791a3836ed2ab562015680
Wei Tan <tanwei0404@163.com>
2023-12-10 11:07:23 +0800

    refactor: Make sentiment.nvim lazy load

--- lua/plugins/edit/sentiment.lua
+++ lua/plugins/edit/sentiment.lua
@@ -8,4 +8,22 @@ M.init = function()
   vim.g.loaded_matchparen = 1
 end
 
+M.opts = {
+  included_buftypes = {
+    [""] = true,
+  },
+  excluded_filetypes = {},
+  included_modes = {
+    n = true,
+    i = true,
+  },
+  delay = 50,
+  limit = 1000,
+  pairs = {
+    { "(", ")" },
+    { "{", "}" },
+    { "[", "]" },
+  },
+}
+
 return M
