652ee391798b5dcaae93c076670a19cbbe56fd95
Wei Tan <tanwei0404@163.com>
2023-12-10 13:07:04 +0800

    refactor: Make reach.nvim lazy load

--- lua/plugins/general/reach.lua
+++ lua/plugins/general/reach.lua
@@ -1,5 +1,12 @@
 local M = {
   "toppair/reach.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  cmd = { "ReachOpen" },
 }
+
+-- TODO: This plugin should write more configurations
+M.config = true
 
 return M
--- lua/user/reach.lua
+++
@@ -1,6 +0,0 @@
-local status_ok, reach = pcall(require, "reach")
-if not status_ok then
-  return
-end
-
-reach.setup()
