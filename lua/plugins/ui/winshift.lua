2f17afae856cc6d00e5f45195f022ff3d899f105
Wei Tan <tanwei0404@163.com>
2023-12-10 10:08:16 +0800

    refactor: Make winshift.nvim lazy load

--- lua/plugins/ui/winshift.lua
+++ lua/plugins/ui/winshift.lua
@@ -1,7 +1,26 @@
 local M = {
   "sindrets/winshift.nvim",
+  cmd = { "WinShift" },
 }
 
 M.init = function() end
+
+M.config = function()
+  require("winshift").setup {
+    focused_hl_group = "Visual",
+    window_picker = function()
+      return require("winshift.lib").pick_window {
+        picker_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+        filter_rules = {
+          cur_win = true,
+          floats = true,
+          filetype = { "NvimTree", "alpha" },
+          buftype = { "nofile", "terminal", "quickfix" },
+        },
+        filter_func = nil,
+      }
+    end,
+  }
+end
 
 return M
