d3e586e5e75b7428476b884e51c2a712cd23a1fd
Wei Tan <tanwei0404@163.com>
2023-12-10 09:48:26 +0800

    refactor: Make comment-box.nvim lazy load

--- lua/plugins/edit/comment-box.lua
+++ lua/plugins/edit/comment-box.lua
@@ -1,7 +1,24 @@
 local M = {
   "LudoPinelli/comment-box.nvim",
+  cmd = {
+    "CBllbox",
+    "CBlcbox",
+    "CBlrbox",
+    "CBclbox",
+    "CBccbox",
+    "CBcrbox",
+    "CBrlbox",
+    "CBrcbox",
+    "CBrrbox",
+    "CBalbox",
+    "CBacbox",
+    "CBarbox",
+  },
 }
 
 M.init = function() end
+
+-- TODO: Configure for comment-box.nvim
+M.config = true
 
 return M
--- lua/user/comment-box.lua
+++
@@ -1,7 +0,0 @@
-local status_ok, comment_box = pcall(require, "comment-box")
-if not status_ok then
-  return
-end
-
--- TODO: Configure for comment-box.nvim
-comment_box.setup()
