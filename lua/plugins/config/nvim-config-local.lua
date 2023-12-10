183c5eaf8496d9961716d0917231ca7a27bd1cbd
Wei Tan <tanwei0404@163.com>
2023-12-10 09:55:47 +0800

    refactor: Make nvim-config-local lazy load

--- lua/plugins/config/nvim-config-local.lua
+++ lua/plugins/config/nvim-config-local.lua
@@ -1,5 +1,21 @@
 local M = {
   "klen/nvim-config-local",
+  config = function()
+    require("config-local").setup {
+      config_files = {
+        ".nvim.lua",
+        ".nvimrc",
+        ".exrc",
+        ".nvim/nvim.lua",
+      },
+      hashfile = vim.fn.stdpath "data" .. "/config-local",
+      autocommands_create = true,
+      commands_create = true,
+      silent = false,
+      lookup_parents = false,
+    }
+  end,
+  event = { "VimEnter" },
 }
 
 M.init = function() end
