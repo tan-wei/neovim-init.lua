a877a3f19071cc8c7f1e3cc1ffc6af6ba03eaee2
Wei Tan <tanwei0404@163.com>
2023-12-10 15:44:54 +0800

    refactor: Make toggletasks.nvim lazy load

--- lua/plugins/task/toggletasks.lua
+++ lua/plugins/task/toggletasks.lua
@@ -5,6 +5,21 @@ local M = {
     "akinsho/toggleterm.nvim",
     "nvim-telescope/telescope.nvim",
   },
+  cmd = { "ToggleTasksInfo", "ToggleTasksConvert" },
+  event = "VeryLazy",
 }
+
+-- TODO: This plugin should write more configurations
+M.config = function()
+  require("toggletasks").setup {
+    silent = true,
+    search_paths = {
+      "toggletasks",
+      ".toggletasks",
+      ".nvim/toggletasks",
+    },
+  }
+  require("telescope").load_extension "toggletasks"
+end
 
 return M
