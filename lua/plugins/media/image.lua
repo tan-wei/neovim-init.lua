350764d0a71f46a4e4fc216ce2a34ac0643e6ee9
Wei Tan <tanwei0404@163.com>
2023-12-11 23:01:09 +0800

    refactor: Make image.nvim lazy load

--- lua/plugins/media/image.lua
+++ lua/plugins/media/image.lua
@@ -4,6 +4,21 @@ local M = {
     "nvim-lua/plenary.nvim",
     "m00qek/baleia.nvim",
   },
+  event = "VeryLazy",
+}
+
+M.opts = {
+  render = {
+    min_padding = 5,
+    show_label = true,
+    show_image_dimensions = true,
+    use_dither = true,
+    foreground_color = true,
+    background_color = true,
+  },
+  events = {
+    update_on_nvim_resize = true,
+  },
 }
 
 return M
