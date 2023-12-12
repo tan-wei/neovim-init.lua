11e6d8093d1fc6f3443773b4999475e1b8ca81d0
Wei Tan <tanwei0404@163.com>
2023-12-12 11:14:45 +0800

    refactor: Make mkdnflow.nvim lazy load

--- lua/plugins/markdown/mkdnflow.lua
+++ lua/plugins/markdown/mkdnflow.lua
@@ -1,5 +1,12 @@
 local M = {
   "jakewvincent/mkdnflow.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  ft = "markdown",
 }
+
+-- TODO: This plugin should write more configurations
+M.opts = {}
 
 return M
