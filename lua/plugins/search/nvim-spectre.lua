139e443151967c2da657d580709326bba77eaf31
Wei Tan <tanwei0404@163.com>
2023-12-12 10:11:09 +0800

    refactor: Make nvim-spectre lazy load

--- lua/plugins/search/nvim-spectre.lua
+++ lua/plugins/search/nvim-spectre.lua
@@ -3,6 +3,15 @@ local M = {
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
+  cmd = "Spectre",
 }
+
+-- TODO: This plugin should write more configurations
+M.config = function()
+  require("spectre").setup {
+    live_update = false,
+    mappings = {},
+  }
+end
 
 return M
