a805c487a42e21e3eb758ff92600bc11238d942d
Wei Tan <tanwei0404@163.com>
2023-12-12 08:28:06 +0800

    refactor: Make crates.nvim lazy load

--- lua/plugins/rust/crates.lua
+++ lua/plugins/rust/crates.lua
@@ -2,7 +2,22 @@ local M = {
   "Saecki/crates.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
   },
+  event = { "BufRead Cargo.toml" },
 }
+
+M.config = function()
+  require("crates").setup()
+
+  local cmp = require "cmp"
+  vim.api.nvim_create_autocmd("BufRead", {
+    group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
+    pattern = "Cargo.toml",
+    callback = function()
+      cmp.setup.buffer { sources = { { name = "crates" } } }
+    end,
+  })
+end
 
 return M
