8b87bcdb9bda491e9ffdd2c5c850a060ee708533
Wei Tan <tanwei0404@163.com>
2023-12-12 08:56:20 +0800

    refactor: Make nvim-scrollview.nvim lazy load

--- lua/plugins/ui/nvim-scrollview.lua
+++ lua/plugins/ui/nvim-scrollview.lua
@@ -1,5 +1,21 @@
 local M = {
   "dstein64/nvim-scrollview",
+  event = "VeryLazy",
 }
+
+M.config = function()
+  require("scrollview").setup {
+    excluded_filetypes = {},
+    current_only = true,
+    signs_on_startup = { "all" },
+    diagnostics_severities = { vim.diagnostic.severity.ERROR },
+  }
+
+  require("scrollview.contrib.gitsigns").setup {
+    add_priority = 100,
+    change_priority = 100,
+    delete_priority = 100,
+  }
+end
 
 return M
--- lua/user/nvim-scrollview.lua
+++
@@ -1,9 +0,0 @@
-local status_ok, scrollview = pcall(require, "scrollview")
-if not status_ok then
-  return
-end
-
-scrollview.setup {
-  excluded_filetypes = {},
-  current_only = true,
-}
