b1d5a4be48f132d5b99732c05f915f847591ba60
Wei Tan <tanwei0404@163.com>
2023-12-13 11:05:19 +0800

    refactor: Make carbon-now.nvim lazy load

--- lua/plugins/media/carbon-now.lua
+++ lua/plugins/media/carbon-now.lua
@@ -4,4 +4,31 @@ local M = {
   cmd = "CarbonNow",
 }
 
+M.config = function()
+  local cmds = {
+    Windows_NT = "google",
+    Linux = "xdg-open",
+    Darwin = "open",
+  }
+
+  require("carbon-now").setup {
+    open_cmd = cmds[vim.uv.os_uname().sysname],
+    options = {
+      bg = "gray",
+      drop_shadow_blur = "68px",
+      drop_shadow = false,
+      drop_shadow_offset_y = "20px",
+      font_family = "Hack",
+      font_size = "18px",
+      line_height = "133%",
+      line_numbers = true,
+      theme = "monokai",
+      titlebar = "Made with carbon-now.nvim",
+      watermark = false,
+      width = "680",
+      window_theme = "sharp",
+    },
+  }
+end
+
 return M
