1ab6e09476ab785f70a819c1b4797b725b652abc
Wei Tan <tanwei0404@163.com>
2023-12-13 10:31:51 +0800

    refactor: Make nvim-neoclip.lua lazy load

--- lua/plugins/register/nvim-neoclip.lua
+++ lua/plugins/register/nvim-neoclip.lua
@@ -2,7 +2,15 @@ local M = {
   "AckslD/nvim-neoclip.lua",
   dependencies = {
     "nvim-telescope/telescope.nvim",
+    { "kkharji/sqlite.lua", module = "sqlite" },
   },
+  event = "VeryLazy",
 }
+
+-- TODO: This plugin should write more configurations
+M.config = function()
+  require("neoclip").setup()
+  require("telescope").load_extension "neoclip"
+end
 
 return M
