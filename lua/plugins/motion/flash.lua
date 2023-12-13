4fd49fc13b4553b2f1764675b7afac3bf3fb540d
Wei Tan <tanwei0404@163.com>
2023-12-13 09:04:51 +0800

    refactor: Make falsh.nvim lazy load

--- lua/plugins/motion/flash.lua
+++ lua/plugins/motion/flash.lua
@@ -3,4 +3,20 @@ local M = {
   event = "VeryLazy",
 }
 
+M.opts = {
+  search = {
+    exclude = {
+      "notify",
+      "cmp_menu",
+      "noice",
+      "flash_prompt",
+      function(win)
+        return not vim.api.nvim_win_get_config(win).focusable
+      end,
+      "NvimTree",
+    },
+  },
+}
+
+
 return M
