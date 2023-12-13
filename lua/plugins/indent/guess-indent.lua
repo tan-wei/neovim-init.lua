b1e6567461a1bc48073604dbf06d8eccfdab823c
Wei Tan <tanwei0404@163.com>
2023-12-13 23:12:23 +0800

    refactor: Make guess-indent.nvim lazy load

--- lua/plugins/indent/guess-indent.lua
+++ lua/plugins/indent/guess-indent.lua
@@ -1,5 +1,22 @@
 local M = {
   "NMAC427/guess-indent.nvim",
+  event = "BufEnter",
+}
+
+M.opts = {
+  auto_cmd = true,
+  override_editorconfig = false,
+  filetype_exclude = {
+    "netrw",
+    "tutor",
+    "NvimTree",
+  },
+  buftype_exclude = {
+    "help",
+    "nofile",
+    "terminal",
+    "prompt",
+  },
 }
 
 return M
