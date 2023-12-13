20459d05d114edea6989cbb3c564b095e93c75cf
Wei Tan <tanwei0404@163.com>
2023-12-13 11:11:21 +0800

    refactor: Make iswap.nvim lazy load

--- lua/plugins/treesitter/iswap.lua
+++ lua/plugins/treesitter/iswap.lua
@@ -3,6 +3,10 @@ local M = {
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
   },
+  cmd = { "ISwap", "ISwapWith", "ISwapNode", "ISwapNodeWith", "IMove" },
 }
+
+-- TODO: This plugin should write more configurations
+M.config = true
 
 return M
