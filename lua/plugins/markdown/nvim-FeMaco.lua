52394088457d73d4ae09741574877e70467a64c5
Wei Tan <tanwei0404@163.com>
2023-12-14 11:43:25 +0800

    refactor: Make nvim-FeMaco.nvim lazy load

--- lua/plugins/markdown/nvim-FeMaco.lua
+++ lua/plugins/markdown/nvim-FeMaco.lua
@@ -1,8 +1,26 @@
 local M = {
   "AckslD/nvim-FeMaco.lua",
+  cmd = "FeMaco",
 }
 
-M.init = function()
+M.config = function()
+  local mapped_filetype = {
+    ["c++"] = "cpp",
+    ["cxx"] = "cpp",
+  }
+
+  require("femaco").setup {
+    ft_from_lang = function(lang)
+      if mapped_filetype[lang] then
+        return mapped_filetype[lang]
+      end
+      return lang
+    end,
+
+    ensure_newline = function(base_filetype)
+      return true
+    end,
+  }
 end
 
 return M
