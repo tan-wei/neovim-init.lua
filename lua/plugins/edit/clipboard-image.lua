17cc462576d8d66feda834743cc1830d043cb504
Wei Tan <tanwei0404@163.com>
2023-12-14 08:49:15 +0800

    refactor: Make clipboard-image.nvim lazy load

--- lua/plugins/edit/clipboard-image.lua
+++ lua/plugins/edit/clipboard-image.lua
@@ -4,6 +4,27 @@ local M = {
   dependencies = {
     "danielwe/vim-percent",
   },
+  ft = { "markdown" },
+}
+
+M.opts = {
+  default = {
+    img_dir = "images",
+    img_name = function()
+      vim.fn.inputsave()
+      local name = vim.fn.input "Name: "
+      vim.fn.inputrestore()
+      return name
+    end,
+    affix = "<\n  %s\n>", -- Multi lines affix
+  },
+
+  markdown = {
+    img_dir = { "%:p:h", "%:t:r" },
+    img_dir_txt = function()
+      return { "./" .. vim.fn["percent#encode"](vim.fn.expand "%:t:r") }
+    end,
+  },
 }
 
 return M
