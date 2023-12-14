b061e68578207725b362b9ec58862e84ab58743f
Wei Tan <tanwei0404@163.com>
2023-12-14 23:08:32 +0800

    refactor: Make cybu.nvim lazy load

--- lua/plugins/ui/cybu.lua
+++ lua/plugins/ui/cybu.lua
@@ -1,5 +1,24 @@
 local M = {
   "ghillb/cybu.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "nvim-lua/plenary.nvim",
+  },
+  event = "VeryLazy",
+}
+
+M.init = function()
+  vim.keymap.set("n", "[b", "<Plug>(CybuPrev)")
+  vim.keymap.set("n", "]b", "<Plug>(CybuNext)")
+  vim.keymap.set("n", "<s-tab>", "<plug>(CybuLastusedPrev)")
+  vim.keymap.set("n", "<tab>", "<plug>(CybuLastusedNext)")
+end
+
+M.opts = {
+  exclude = {
+    "fugitive",
+    "qf",
+  },
 }
 
 return M
