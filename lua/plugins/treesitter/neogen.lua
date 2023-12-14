e44a250a5763f88293db6825eea9ad0610385aea
Wei Tan <tanwei0404@163.com>
2023-12-14 23:21:39 +0800

    refactor: Make neogen lazy load

--- lua/plugins/treesitter/neogen.lua
+++ lua/plugins/treesitter/neogen.lua
@@ -2,7 +2,19 @@ local M = {
   "danymat/neogen",
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
+    "L3MON4D3/LuaSnip",
   },
+  cmd = "Neogen",
 }
+
+M.config = function()
+  require("neogen").setup {
+    snippet_engine = "luasnip",
+    languages = {
+      ["cpp.doxygen"] = require "neogen.configurations.cpp",
+      ["c.doxygen"] = require "neogen.configurations.c",
+    },
+  }
+end
 
 return M
