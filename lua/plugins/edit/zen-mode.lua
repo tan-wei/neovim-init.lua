5b503fe6515c8ffe27de67777b7db047c6b7e801
Wei Tan <tanwei0404@163.com>
2023-12-14 13:17:04 +0800

    refactor: Make zen-mode.nvim lazy load

--- lua/plugins/edit/zen-mode.lua
+++ lua/plugins/edit/zen-mode.lua
@@ -1,5 +1,14 @@
 local M = {
   "folke/zen-mode.nvim",
+  dependencies = {
+    "folke/twilight.nvim",
+  },
+  cmd = "ZenMode",
 }
+
+-- TODO: This plugin should write more configurations, especially for wezterm, kitty
+M.config = function()
+  require("zen-mode").setup()
+end
 
 return M
