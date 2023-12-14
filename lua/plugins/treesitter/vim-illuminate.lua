956b8fd0467af4dcf034c842eb8ae43785a127ac
Wei Tan <tanwei0404@163.com>
2023-12-14 15:52:27 +0800

    refactor: Make vim-illuminate.lua lazy load

--- lua/plugins/treesitter/vim-illuminate.lua
+++ lua/plugins/treesitter/vim-illuminate.lua
@@ -1,5 +1,38 @@
 local M = {
   "RRethy/vim-illuminate",
+  event = "VeryLazy",
 }
+
+M.config = function()
+  local status_ok, illuminate = pcall(require, "illuminate")
+  if not status_ok then
+    return
+  end
+
+  require("illuminate").configure {
+    providers = {
+      "lsp",
+      "treesitter",
+      "regex",
+    },
+    delay = 100,
+    filetypes_denylist = {
+      "dirvish",
+      "fugitive",
+    },
+  }
+
+  vim.cmd [[
+    augroup _illuminate
+      autocmd!
+      autocmd VimEnter * hi link illuminatedWord CursorLine
+    augroup END
+
+    augroup _illuminate
+      autocmd!
+      autocmd VimEnter * hi illuminatedWord cterm=underline gui=underline
+    augroup END
+  ]]
+end
 
 return M
