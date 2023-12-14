e26f4f06262b98382215518e31a633766f0be2d9
Wei Tan <tanwei0404@163.com>
2023-12-14 11:50:31 +0800

    refactor: Make godbolt.nvim lazy load

--- lua/plugins/media/godbolt.lua
+++ lua/plugins/media/godbolt.lua
@@ -1,5 +1,19 @@
 local M = {
   "p00f/godbolt.nvim",
+  cmd = { "Godbolt", "GodboltCompiler" },
+}
+
+M.opts = {
+  languages = {
+    cpp = { compiler = "clang1700", options = {} },
+    c = { compiler = "clang1700", options = {} },
+    rust = { compiler = "nightly", options = {} },
+  },
+  quickfix = {
+    enable = true,
+    auto_open = true,
+  },
+  url = "https://godbolt.org", -- can be changed to a different godbolt instance
 }
 
 return M
