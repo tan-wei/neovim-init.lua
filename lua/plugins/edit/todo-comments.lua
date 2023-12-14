675311d65d6d36f7f3967a6cc43718ffac60df34
Wei Tan <tanwei0404@163.com>
2023-12-14 22:46:47 +0800

    refactor: Make todo-comments.nvim lazy load

--- lua/plugins/edit/todo-comments.lua
+++ lua/plugins/edit/todo-comments.lua
@@ -1,6 +1,33 @@
 local M = {
   "folke/todo-comments.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "folke/trouble.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  cmd = "VeryLazy",
+}
+
+M.opts = {
+  keywords = {
+    FIX = {
+      icon = " ",
+      color = "error",
+      alt = { "FIXME", "BUG", "FIXIT", "ISSUE" },
+    },
+    TODO = { icon = "", color = "info" },
+    HACK = { icon = " ", color = "warning" },
+    WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+    PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+    NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+    TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+  },
+  highlight = {
+    exclude = {},
+  },
+  search = {
+    pattern = [[\b(KEYWORDS):+]],
+  },
 }
 
 return M
