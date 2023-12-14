0faadfaf29ae8396e60d1437776afb42e03ca617
Wei Tan <tanwei0404@163.com>
2023-12-14 22:56:09 +0800

    refactor: Make whitespace.nvim lazy load

--- lua/plugins/edit/whitespace.lua
+++ lua/plugins/edit/whitespace.lua
@@ -1,5 +1,28 @@
 local M = {
   "johnfrankmorgan/whitespace.nvim",
+  event = "VeryLazy",
 }
+
+M.config = function()
+  require("whitespace-nvim").setup {
+    highlight = "DiffDelete",
+    ignored_filetypes = {
+      "TelescopePrompt",
+      "TelescopeResults",
+      "Trouble",
+      "help",
+      "alpha",
+      "toggleterm",
+      "WhichKey",
+      "checkhealth",
+      "notify",
+      "noice",
+      "lspinfo",
+      "lazy",
+      "Outline",
+    },
+    ignore_terminal = true,
+  }
+end
 
 return M
