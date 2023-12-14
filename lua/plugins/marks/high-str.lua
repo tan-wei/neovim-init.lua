ccaa0d27d4c3e1fc9c0f5494c38db82b00ed8c18
Wei Tan <tanwei0404@163.com>
2023-12-14 15:36:48 +0800

    refactor: Make high-str.lua lazy load

--- lua/plugins/marks/high-str.lua
+++ lua/plugins/marks/high-str.lua
@@ -1,5 +1,24 @@
 local M = {
   "pocco81/high-str.nvim",
+  cmd = { "HSHighlight", "HSRmHighlight", "HSExport", "HSImport" },
+}
+
+M.opts = {
+  verbosity = 0,
+  saving_path = vim.fn.stdpath "data" .. "/highstr/",
+  highlight_colors = {
+    -- color_id = {"bg_hex_code",<"fg_hex_code"/"smart">}
+    color_0 = { "#0c0d0e", "smart" }, -- Cosmic charcoal
+    color_1 = { "#e5c07b", "smart" }, -- Pastel yellow
+    color_2 = { "#7FFFD4", "smart" }, -- Aqua menthe
+    color_3 = { "#8A2BE2", "smart" }, -- Proton purple
+    color_4 = { "#FF4500", "smart" }, -- Orange red
+    color_5 = { "#008000", "smart" }, -- Office green
+    color_6 = { "#0000FF", "smart" }, -- Just blue
+    color_7 = { "#FFC0CB", "smart" }, -- Blush pink
+    color_8 = { "#FFF9E3", "smart" }, -- Cosmic latte
+    color_9 = { "#7d5c34", "smart" }, -- Fallow brown
+  },
 }
 
 return M
