fd5e6415e27ec2c17cf38cc1b3eab8d14a19377b
Wei Tan <tanwei0404@163.com>
2023-12-15 22:28:30 +0800

    refactor: Make diffview.nvim lazy load

--- lua/plugins/git/diffview.lua
+++ lua/plugins/git/diffview.lua
@@ -1,5 +1,17 @@
 local M = {
   "sindrets/diffview.nvim",
+  cmd = {
+    "DiffviewOpen",
+    "DiffviewFileHistory",
+    "DiffviewClose",
+    "DiffviewToggleFiles",
+    "DiffviewFocusFiles",
+    "DiffviewRefresh",
+    "DiffviewLog",
+  },
 }
+
+-- TODO: This plugin should write more configurations
+M.config = true
 
 return M
