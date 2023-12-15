62c7e874eebf8951c692d3243d4f5d10af2cb64a
Wei Tan <tanwei0404@163.com>
2023-12-15 13:33:19 +0800

    refactor: Make indent-blankline.nvim lazy load

--- lua/plugins/indent/indent-blankline.lua
+++ lua/plugins/indent/indent-blankline.lua
@@ -3,24 +3,28 @@ local M = {
   event = "VeryLazy",
 }
 
-M.config = function()
-  local ibl = require "ibl"
-  local highlight = {
-    "RainbowRed",
-    "RainbowYellow",
-    "RainbowBlue",
-    "RainbowOrange",
-    "RainbowGreen",
-    "RainbowViolet",
-    "RainbowCyan",
-  }
+-- TODO: This table should import by user config, and shared with other plugins (e.g.,rainbowdelimiters)
+local highlight = {
+  "RainbowRed",
+  "RainbowYellow",
+  "RainbowBlue",
+  "RainbowOrange",
+  "RainbowGreen",
+  "RainbowViolet",
+  "RainbowCyan",
+}
 
+M.init = function()
   vim.opt.list = true
   vim.opt.listchars:append "space:⋅"
   vim.opt.listchars:append "eol:↴"
   vim.g.rainbow_delimiters = { highlight = highlight }
+end
 
+M.config = function()
+  local ibl = require "ibl"
   local hooks = require "ibl.hooks"
+
   -- create the highlight groups in the highlight setup hook, so they are reset
   -- every time the colorscheme changes
   hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
