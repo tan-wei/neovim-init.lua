8f49414c6f281e3721e4cdf0b5a7f98d83bb141b
Wei Tan <tanwei0404@163.com>
2023-12-15 22:19:59 +0800

    refactor: Make colorscheme-randomizer.nvim lazy load

--- lua/plugins/general/colorscheme-randomizer.lua
+++ lua/plugins/general/colorscheme-randomizer.lua
@@ -3,4 +3,13 @@ local M = {
   tag = "v1.0.0",
 }
 
+M.config = function()
+  require("colorscheme-randomizer").setup {
+    apply_scheme = true,
+    plugin_strategy = "lazy",
+    plugins = vim.g.available_colorschemes,
+    colorschemes = vim.g.available_colorschemes,
+  }
+end
+
 return M
