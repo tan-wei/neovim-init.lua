359b43a5295d428a1cd7ed88d26157b1f4f43c3d
Wei Tan <tanwei0404@163.com>
2023-12-15 14:59:47 +0800

    refactor: Make overseer.nvim lazy load

--- lua/plugins/code-runner/overseer.lua
+++ lua/plugins/code-runner/overseer.lua
@@ -1,5 +1,15 @@
 local M = {
   "stevearc/overseer.nvim",
+  event = "VeryLazy",
 }
+
+-- TODO: Conifgure for several third-party integrations, such as lualine, neotest, toggleterm, session
+M.config = function()
+  local overseer = require "overseer"
+
+  overseer.register_template(require "user.overseer.template.run_script")
+
+  overseer.setup()
+end
 
 return M
--- lua/user/overseer/init.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, overseer = pcall(require, "overseer")
-if not status_ok then
-  return
-end
-
-overseer.register_template(require "user.overseer.template.run_script")
-
-overseer.setup()
