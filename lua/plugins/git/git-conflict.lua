e0b34148f3729a25c2668e90cf343d88786ba26e
Wei Tan <tanwei0404@163.com>
2023-12-16 12:46:59 +0800

    refactor: Make git-conflict.nvim lazy load

--- lua/plugins/git/git-conflict.lua
+++ lua/plugins/git/git-conflict.lua
@@ -1,5 +1,17 @@
 local M = {
   "akinsho/git-conflict.nvim",
+  cmd = {
+    "GitConflictChooseOurs",
+    "GitConflictChooseTheirs",
+    "GitConflictChooseBoth",
+    "GitConflictChooseNone",
+    "GitConflictNextConflict",
+    "GitConflictPrevConflict",
+    "GitConflictListQf",
+  },
 }
+
+-- TODO: This plugin should write more configurations
+M.config = true
 
 return M
--- lua/user/git-conflict.lua
+++
@@ -1,7 +0,0 @@
-local status_ok, git_conflict = pcall(require, "git-conflict")
-if not status_ok then
-  return
-end
-
--- TODO: Configure for git-conflict.nvim
-git_conflict.setup()
