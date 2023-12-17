0b3c42c6e2d473a57181e45a383b31a43494a2fc
Wei Tan <tanwei0404@163.com>
2023-12-17 12:23:19 +0800

    refactor: Make nvim-lint lazy load

--- lua/plugins/linter/nvim-lint.lua
+++ lua/plugins/linter/nvim-lint.lua
@@ -1,5 +1,13 @@
 local M = {
   "mfussenegger/nvim-lint",
+  event = "BufEnter",
 }
+
+-- TODO: Add lint progress by require("lint").get_running()
+M.config = function()
+  require("lint").linters_by_ft = {
+    cpp = { "clangtidy", "cppcheck" },
+  }
+end
 
 return M
--- lua/user/linter.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, lint = pcall(require, "lint")
-if not status_ok then
-  return
-end
-
-lint.linters_by_ft = {
-  cpp = { "clangtidy", "cppcheck" },
-}
