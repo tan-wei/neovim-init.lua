22ca78b499debab0f9c1be8bd0439b8a6d9550f7
Wei Tan <tanwei0404@163.com>
2023-12-17 18:44:26 +0800

    refactor: Make lspsaga.nvim lazy load

--- lua/plugins/lsp/lspsaga.lua
+++ lua/plugins/lsp/lspsaga.lua
@@ -4,6 +4,17 @@ local M = {
     "nvim-treesitter/nvim-treesitter",
     "nvim-tree/nvim-web-devicons",
   },
+  event = "LspAttach",
+}
+
+M.opts = {
+  ui = {
+    code_action = "",
+  },
+
+  symbol_in_winbar = {
+    enable = false,
+  },
 }
 
 return M
