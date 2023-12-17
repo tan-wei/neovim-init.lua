531e377d9713925a3eb2849e0fb54e8d17176413
Wei Tan <tanwei0404@163.com>
2023-12-17 11:19:50 +0800

    refactor: Make nvim-hlslens lazy load

--- lua/plugins/search/nvim-hlslens.lua
+++ lua/plugins/search/nvim-hlslens.lua
@@ -1,5 +1,11 @@
 local M = {
   "kevinhwang91/nvim-hlslens",
+  event = "VeryLazy",
 }
+
+-- TODO: This plugin should write more configurations
+M.config = function()
+  require("hlslens").setup()
+end
 
 return M
