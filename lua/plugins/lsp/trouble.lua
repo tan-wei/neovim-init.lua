0620e7e75a0f55078680143b1c7523dac42ee9bd
Wei Tan <tanwei0404@163.com>
2023-12-17 19:26:52 +0800

    refactor: Make trouble.nvim lazy load

--- lua/plugins/lsp/trouble.lua
+++ lua/plugins/lsp/trouble.lua
@@ -1,5 +1,12 @@
 local M = {
   "folke/trouble.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  cmd = { "Trouble", "TroubleClose", "TroubleToggle", "TroubleRefresh" },
 }
+
+-- TODO: This plugin should write more configurations
+M.config = true
 
 return M
