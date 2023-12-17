6e248bc7f040e0aeafc5adc2eb11ccac0ba9ce95
Wei Tan <tanwei0404@163.com>
2023-12-17 11:47:00 +0800

    refactor: Make windows.nvim lazy load

--- lua/plugins/ui/windows.lua
+++ lua/plugins/ui/windows.lua
@@ -4,6 +4,51 @@ local M = {
     "anuvyklack/middleclass",
     "anuvyklack/animation.nvim",
   },
+  cmd = {
+    "WindowsMaximize",
+    "WindowsMaximizeVertically",
+    "WindowsMaximizeHorizontally",
+    "WindowsEqualize",
+    "WindowsEnableAutowidth",
+    "WindowsDisableAutowidth",
+    "WindowsToggleAutowidth",
+  },
 }
+
+M.config = function()
+  require("windows").setup {
+    autowidth = {
+      enable = false,
+      winwidth = 5,
+      filetype = {},
+    },
+    ignore = {
+      buftype = {
+        "quickfix",
+      },
+      filetype = {
+        "NvimTree",
+        "neo-tree",
+        "undotree",
+        "gundo",
+      },
+    },
+    animation = {
+      enable = true,
+      duration = 300,
+      fps = 30,
+      easing = "in_out_sine",
+    },
+  }
+
+  local function cmd(command)
+    return table.concat { "<Cmd>", command, "<CR>" }
+  end
+
+  vim.keymap.set("n", "<C-w>z", cmd "WindowsMaximize")
+  vim.keymap.set("n", "<C-w>_", cmd "WindowsMaximizeVertically")
+  vim.keymap.set("n", "<C-w>|", cmd "WindowsMaximizeHorizontally")
+  vim.keymap.set("n", "<C-w>=", cmd "WindowsEqualize")
+end
 
 return M
