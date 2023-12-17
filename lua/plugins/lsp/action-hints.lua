958bc7a82a5dd003e149bee83776e2c16324ab41
Wei Tan <tanwei0404@163.com>
2023-12-17 17:11:02 +0800

    refactor: action-hints.nvim lazy load

--- lua/plugins/lsp/action-hints.lua
+++ lua/plugins/lsp/action-hints.lua
@@ -1,5 +1,8 @@
 local M = {
   "roobert/action-hints.nvim",
+  event = "LspAttach",
 }
+
+M.config = true
 
 return M
--- lua/user/lsp/action-hints.lua
+++
@@ -1,6 +0,0 @@
-local status_ok, action_hints = pcall(require, "action-hints")
-if not status_ok then
-  return
-end
-
-action_hints.setup()
