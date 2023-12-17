5c83c0da16584d93f587c4dc7115a80398b1aad1
Wei Tan <tanwei0404@163.com>
2023-12-17 17:14:16 +0800

    refactor: Make glance.nvim lazy load

--- lua/plugins/lsp/glance.lua
+++ lua/plugins/lsp/glance.lua
@@ -1,5 +1,9 @@
 local M = {
   "dnlhc/glance.nvim",
+  cmd = "Galance",
 }
+
+-- TODO: This plugin should write more configurations
+M.config = true
 
 return M
--- lua/user/lsp/glance.lua
+++
@@ -1,6 +0,0 @@
-local status_ok, glance = pcall(require, "glance")
-if not status_ok then
-  return
-end
-
-glance.setup()
