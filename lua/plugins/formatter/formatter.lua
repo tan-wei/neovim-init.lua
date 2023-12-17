9b79a70460f9083fe555d5e368bc2b4f890b6ecd
Wei Tan <tanwei0404@163.com>
2023-12-17 11:58:39 +0800

    refactor: Make formatter.nvim lazy load

--- lua/plugins/formatter/formatter.lua
+++ lua/plugins/formatter/formatter.lua
@@ -3,4 +3,37 @@ local M = {
   enabled = false,
 }
 
+M.config = function()
+  local util = require "formatter.util"
+
+  require("formmater").setup {
+    logging = true,
+    log_level = vim.log.levels.WARN,
+    filetype = {
+      lua = {
+        require("formatter.filetypes.lua").stylua,
+      },
+      c = {
+        require("formatter.filetypes.c").clangformat,
+      },
+      cpp = {
+        require("formatter.filetypes.c").clangformat,
+      },
+      rust = {
+        require("formatter.filetypes.rust").rustfmt,
+      },
+      ["*"] = {
+        require("formatter.filetypes.any").remove_trailing_whitespace,
+      },
+    },
+  }
+
+  vim.cmd [[
+     augroup _auto_format
+       autocmd!
+       autocmd BufWritePost * FormatWrite
+     augroup END
+  ]]
+end
+
 return M
