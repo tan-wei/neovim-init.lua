d2b7741bf1e413ce3b154ac03ebb57fbd7bfd4b9
Wei Tan <tanwei0404@163.com>
2023-12-17 17:26:19 +0800

    refactor: Make aerial.nvim lazy load

--- lua/plugins/lsp/aerial.lua
+++ lua/plugins/lsp/aerial.lua
@@ -4,6 +4,23 @@ local M = {
     "nvim-treesitter/nvim-treesitter",
     "nvim-tree/nvim-web-devicons",
   },
+  cmd = {
+    "AerialToggle",
+    "AerialOpen",
+    "AerialOpenAll",
+    "AerialClose",
+    "AerialCloseAll",
+    "AerialNext",
+    "AerialPrev",
+    "AerialGo",
+    "AerialInfo",
+    "AerialNavToggle",
+    "AerialNavOpen",
+    "AerialNavClose",
+  },
 }
+
+-- TODO: This plugin should write more configurations
+M.config = true
 
 return M
