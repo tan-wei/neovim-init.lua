7c4c7c48f862a5c039fa08cb808aae5cc889c17f
Wei Tan <tanwei0404@163.com>
2023-12-17 17:16:25 +0800

    refactor: Make lsp_signature.nvim lazy load

--- lua/plugins/lsp/lsp_signature.lua
+++ lua/plugins/lsp/lsp_signature.lua
@@ -3,4 +3,14 @@ local M = {
   event = "VeryLazy",
 }
 
+M.config = function()
+  require("lsp_signature").setup {
+    log_path = vim.fn.stdpath "cache" .. "/lsp_signature.log",
+    debug = true,
+    handler_opts = { border = "rounded" },
+    max_width = 80,
+    noice = false, -- TODO: Will cause error, and the author consider to deprecate this option
+  }
+end
+
 return M
