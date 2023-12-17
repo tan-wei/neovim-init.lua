76f68f76fcf4ebb1f2f8150aad41a287cc9149f7
Wei Tan <tanwei0404@163.com>
2023-12-17 13:38:25 +0800

    refactor: nvim-autopairs lazy load

--- lua/plugins/edit/nvim-autopairs.lua
+++ lua/plugins/edit/nvim-autopairs.lua
@@ -1,6 +1,49 @@
 local M = {
   "windwp/nvim-autopairs",
+  dependencies = {
+    "hrsh7th/nvim-cmp",
+  },
   event = "InsertEnter",
 }
+
+M.config = function()
+  local npairs = require "nvim-autopairs"
+  local Rule = require "nvim-autopairs.rule"
+  local cond = require "nvim-autopairs.conds"
+
+  npairs.setup {
+    check_ts = true,
+    ts_config = {
+      lua = { "string", "source" },
+      javascript = { "string", "template_string" },
+      java = false,
+    },
+    disable_filetype = { "TelescopePrompt", "spectre_panel" },
+    fast_wrap = {
+      map = "<M-e>",
+      chars = { "{", "[", "(", '"', "'" },
+      pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+      offset = 0, -- Offset from pattern match
+      end_key = "$",
+      keys = "qwertyuiopzxcvbnmasdfghjkl",
+      check_comma = true,
+      highlight = "PmenuSel",
+      highlight_grey = "LineNr",
+    },
+  }
+
+  -- TODO: Add rules here
+  npairs.add_rules {}
+
+  local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+  require("cmp").event:on(
+    "confirm_done",
+    cmp_autopairs.on_confirm_done {
+      map_char = {
+        tex = "",
+      },
+    }
+  )
+end
 
 return M
