d1eb01926ea388b755b080900e6599284561968d
Wei Tan <tanwei0404@163.com>
2023-12-19 23:02:40 +0800

    plugin: Add specs.nvim

---
+++ lua/plugins/ui/specs.lua
@@ -0,0 +1,26 @@
+local M = {
+  "edluffy/specs.nvim",
+  event = "VeryLazy",
+}
+
+M.config = function()
+  require("specs").setup {
+    show_jumps = true,
+    min_jump = 30,
+    popup = {
+      delay_ms = 0,
+      inc_ms = 10,
+      blend = 10,
+      width = 10,
+      winhl = "PMenu",
+      fader = require("specs").pulse_fader,
+      resizer = require("specs").shrink_resizer,
+    },
+    ignore_filetypes = {},
+    ignore_buftypes = {
+      nofile = true,
+    },
+  }
+end
+
+return M
