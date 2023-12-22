1e773f162dec3c371c90e063c969b7c6fec63fbe
Wei Tan <tanwei0404@163.com>
2023-12-22 11:14:34 +0800

    colorscheme: Add kimbox

---
+++ lua/plugins/colorscheme/kimbox.lua
@@ -0,0 +1,16 @@
+local M = {
+  "lmburns/kimbox",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "kimbox")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+M.config = function()
+  require("kimbox").load()
+end
+
+return M
