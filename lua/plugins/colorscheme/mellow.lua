5ce0e3ff1c71051c8e4210ad012b7f51f718b5d6
Wei Tan <tanwei0404@163.com>
2023-12-23 16:35:53 +0800

    colorscheme: mellow.nvim has been moved to community repo

--- lua/plugins/colorscheme/mellow.lua
+++ lua/plugins/colorscheme/mellow.lua
@@ -1,9 +1,20 @@
 local M = {
-  "kvrohit/mellow.nvim",
+  "mellow-theme/mellow.nvim",
   lazy = true,
 }
 
 M.init = function()
+  vim.g.mellow_italic_comments = true
+  vim.g.mellow_italic_keywords = true
+  vim.g.mellow_italic_booleans = true
+  vim.g.mellow_italic_functions = true
+  vim.g.mellow_italic_variables = true
+  vim.g.mellow_bold_comments = true
+  vim.g.mellow_bold_keywords = true
+  vim.g.mellow_bold_booleans = true
+  vim.g.mellow_bold_functions = true
+  vim.g.mellow_bold_variables = true
+  vim.g.mellow_transparent = false
   local available_colorschemes = vim.g.available_colorschemes or {}
   table.insert(available_colorschemes, "mellow")
   vim.g.available_colorschemes = available_colorschemes
