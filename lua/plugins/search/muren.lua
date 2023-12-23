b47e63cd096257be48c8a4151d0f729ca421909f
Wei Tan <tanwei0404@163.com>
2023-12-23 14:48:13 +0800

    plugin: Add muren.nvim

---
+++ lua/plugins/search/muren.lua
@@ -0,0 +1,9 @@
+local M = {
+  "AckslD/muren.nvim",
+  cmd = { "MurenToggle", "MurenOpen", "MurenClose", "MurenFresh", "MurenFresh", "MurenUnique" },
+}
+
+-- TODO: This plugin should write more configurations
+M.config = true
+
+return M
