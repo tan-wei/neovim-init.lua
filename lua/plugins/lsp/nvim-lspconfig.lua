5b7a6174614f63e2b285d1fc051cf2f58008987d
Wei Tan <tanwei0404@163.com>
2023-12-24 09:34:19 +0800

    config: Fix nvim-lspconfig loading bug

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -3,6 +3,10 @@ local M = {
   dependencies = {
     {
       "antosha417/nvim-lsp-file-operations",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-tree.lua",
+      },
       config = true,
     },
     { "hrsh7th/cmp-nvim-lsp" },
@@ -29,10 +33,9 @@ local servers = {
 
 M.config = function()
   local lspconfig = require "lspconfig"
-  local opts = {}
 
   for _, server in pairs(servers) do
-    opts = {
+    local opts = {
       on_attach = require("user.lsp.handlers").on_attach,
       capabilities = require("user.lsp.handlers").capabilities,
     }
