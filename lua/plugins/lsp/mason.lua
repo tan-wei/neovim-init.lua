922b140927d15416e3b7f1bbf8eabfaea2bff8b3
Wei Tan <tanwei0404@163.com>
2023-12-24 09:17:21 +0800

    config: Fix nvim-lspconfig loading bug

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -2,29 +2,11 @@ local M = {
   "williamboman/mason.nvim",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
   },
   event = "VeryLazy",
 }
 
 M.config = function()
-  local servers = {
-    "lua_ls",
-    -- "cssls",
-    -- "html",
-    -- "tsserver",
-    "pyright",
-    -- "bashls",
-    "jsonls",
-    -- "yamlls",
-    "clangd",
-    -- "ccls",
-    "ltex",
-    -- "rust_analyzer", -- rust-tools.nvim automatically sets up nvim-lspconfig for rust_analyzer
-    "marksman",
-    "cmake",
-  }
-
   local ensure_installed_servers = {
     "lua_ls",
     -- "cssls",
@@ -59,25 +41,6 @@ M.config = function()
     ensure_installed = ensure_installed_servers,
     automatic_installation = true,
   }
-
-  local lspconfig = require "lspconfig"
-
-  local opts = {}
-
-  for _, server in pairs(servers) do
-    opts = {
-      on_attach = require("user.lsp.handlers").on_attach,
-      capabilities = require("user.lsp.handlers").capabilities,
-    }
-
-    server = vim.split(server, "@")[1]
-
-    local require_ok, conf_opts = pcall(require, "user.lsp.settings." .. server)
-    if require_ok then
-      opts = vim.tbl_deep_extend("force", conf_opts, opts)
-    end
-    lspconfig[server].setup(opts)
-  end
 end
 
 return M
---
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -0,0 +1,43 @@
+local M = {
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+}
+
+local servers = {
+  "lua_ls",
+  -- "cssls",
+  -- "html",
+  -- "tsserver",
+  "pyright",
+  -- "bashls",
+  "jsonls",
+  -- "yamlls",
+  "clangd",
+  -- "ccls",
+  "ltex",
+  -- "rust_analyzer", -- rust-tools.nvim automatically sets up nvim-lspconfig for rust_analyzer
+  "marksman",
+  "cmake",
+}
+
+M.config = function()
+  local lspconfig = require "lspconfig"
+  local opts = {}
+
+  for _, server in pairs(servers) do
+    opts = {
+      on_attach = require("user.lsp.handlers").on_attach,
+      capabilities = require("user.lsp.handlers").capabilities,
+    }
+
+    server = vim.split(server, "@")[1]
+
+    local require_ok, conf_opts = pcall(require, "user.lsp.settings." .. server)
+    if require_ok then
+      opts = vim.tbl_deep_extend("force", conf_opts, opts)
+    end
+    lspconfig[server].setup(opts)
+  end
+end
+
+return M
