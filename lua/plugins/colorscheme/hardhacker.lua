83afc4eaaa8b9d5c6fc05ec1b9ee91a12f5dddea
Wei Tan <tanwei0404@163.com>
2023-12-27 10:47:25 +0800

    colorscheme: Add hardhacker

---
+++ lua/plugins/colorscheme/hardhacker.lua
@@ -0,0 +1,13 @@
+local M = {
+  "hardhackerlabs/theme-vim",
+  name = "hardhacker",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "hardhacker")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
