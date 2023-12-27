8515af4b45f96fe8bb411776c2537c8240284045
Wei Tan <tanwei0404@163.com>
2023-12-27 13:07:35 +0800

    plugin: Add vim-header

---
+++ lua/plugins/edit/vim-header.lua
@@ -0,0 +1,17 @@
+local M = {
+  "alpertuna/vim-header",
+  cmd = { "AddHeader", "AddMinHeader" },
+}
+
+M.init = function()
+  vim.g.header_auto_add_header = 0
+  vim.g.header_auto_update_header = 0
+  vim.g.header_field_filename = 1
+  vim.g.header_field_project = "TODO: Project Name"
+  vim.g.header_field_author = "Winterreise"
+  vim.g.header_field_author_email = "winterreise.tanwei@gmail.com"
+  vim.g.header_field_timestamp = 1
+  vim.g.header_field_modified_by = 1
+end
+
+return M
