70c3ff847aaf84b8451f93ec9fbad8ec37348c20
Wei Tan <tanwei0404@163.com>
2023-12-28 21:40:44 +0800

    plugin: Add telescope-zoxide

---
+++ lua/plugins/telescope/telescope-zoxide.lua
@@ -0,0 +1,15 @@
+local M = {
+  "jvgrootveld/telescope-zoxide",
+  dependencies = {
+    "nvim-lua/popup.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  cmd = "Telescope zoxide list",
+}
+
+M.config = function()
+  require("telescope").load_extension "zoxide"
+end
+
+return M
