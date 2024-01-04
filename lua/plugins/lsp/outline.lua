778a01d9aa24a90ff6ae9e73233d9c4994daf9d1
Wei Tan <tanwei0404@163.com>
2024-01-04 08:31:04 +0800

    plugin: Use outline.nvim instead of symbols-outline.nvim

---
+++ lua/plugins/lsp/outline.lua
@@ -0,0 +1,9 @@
+local M = {
+  "hedyhli/outline.nvim",
+  cmd = { "Outline", "OutlineStatus" },
+}
+
+-- TODO: This plugin should write more configurations
+M.config = true
+
+return M
--- lua/plugins/lsp/symbols-outline.lua
+++
@@ -1,8 +0,0 @@
-local M = {
-  "simrat39/symbols-outline.nvim",
-  cmd = { "SymbolsOutline", "SymbolsOutlineOpen", "SymbolsOutlineClose" },
-}
-
-M.config = true
-
-return M
