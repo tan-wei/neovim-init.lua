1357348365048e9470e127b3494ed35e4f32ff42
Wei Tan <tanwei0404@163.com>
2024-01-17 15:46:13 +0800

    colorscheme: Use NeoSolarized.nvim instead of NeoSolarized

--- lua/plugins/colorscheme/NeoSolarized.lua
+++ lua/plugins/colorscheme/NeoSolarized.lua
@@ -1,17 +1,9 @@
 local M = {
-  "overcache/NeoSolarized",
+  "Tsuzat/NeoSolarized.nvim",
   lazy = true,
 }
 
 M.init = function()
-  vim.g.neosolarized_contrast = "normal"
-  vim.g.neosolarized_visibility = "normal"
-  vim.g.neosolarized_vertSplitBgTrans = 1
-  vim.g.neosolarized_bold = 1
-  vim.g.neosolarized_underline = 1
-  vim.g.neosolarized_italic = 1
-  vim.g.neosolarized_termBoldAsBright = 1
-
   local available_colorschemes = vim.g.available_colorschemes or {}
   table.insert(available_colorschemes, "NeoSolarized")
   vim.g.available_colorschemes = available_colorschemes
