b32112dfe872cbf00a73054d1d26558c26c3993b
Wei Tan <tanwei0404@163.com>
2024-01-20 23:45:24 +0800

    colorscheme: Add min-theme.nvim

---
+++ lua/plugins/colorscheme/min-theme.lua
@@ -0,0 +1,14 @@
+local M = {
+  "datsfilipe/min-theme.nvim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "min-theme")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+M.config = true
+
+return M
