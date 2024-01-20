92dc4bc3b2bf6066a122a09e80c0f2fa417f62fd
Wei Tan <tanwei0404@163.com>
2024-01-20 19:09:44 +0800

    colorscheme: Add the-matrix.nvim

---
+++ lua/plugins/colorscheme/the-matrix.lua
@@ -0,0 +1,12 @@
+local M = {
+  "luisiacc/the-matrix.nvim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "thematrx")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
