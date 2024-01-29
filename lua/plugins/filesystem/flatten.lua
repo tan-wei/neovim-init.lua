940a653b8441de9899824dc28e3d7fb0800d6353
Wei Tan <tanwei0404@163.com>
2024-01-29 13:59:15 +0800

    config: Try to fix remote wezterm support for flatten.nvim

--- lua/plugins/filesystem/flatten.lua
+++ lua/plugins/filesystem/flatten.lua
@@ -3,12 +3,60 @@ local M = {
   lazy = false,
   priority = 1001,
 }
+local try_address = function(addr, startserver)
+  if not addr:find "/" then
+    addr = ("%s/%s"):format(vim.fn.stdpath "run", addr)
+  end
+  if vim.loop.fs_stat(addr) then
+    local ok, sock = require("flatten.guest").sockconnect(addr)
+    if ok then
+      return sock
+    end
+  elseif startserver then
+    local ok = pcall(vim.fn.serverstart, addr)
+    if ok then
+      return addr
+    end
+  end
+end
 
 M.config = function()
+  local is_kitty = require("util.client").is_kitty()
+  local is_wezterm = require("util.client").is_wezterm()
+
   require("flatten").setup {
+    pipe_path = function()
+      -- If running in a terminal inside Neovim:
+      if vim.env.NVIM then
+        return vim.env.NVIM
+      end
+
+      if is_kitty and vim.env.KITTY_PID then
+        local ret = try_address("kitty.nvim-" .. vim.env.KITTY_PID, true)
+        if ret ~= nil then
+          return ret
+        end
+      end
+
+      if is_wezterm and vim.env.WEZTERM_UNIX_SOCKET then
+        local pid = vim.env.WEZTERM_UNIX_SOCKET:match "gui%-sock%-(%d+)"
+        if pid then
+          local ret = try_address("wezterm.nvim-" .. pid, true)
+          if ret ~= nil then
+            return ret
+          end
+        else
+          local remote_unix_socket = vim.env.WEZTERM_UNIX_SOCKET
+          local ret = try_address(remote_unix_socket, true)
+          if ret ~= nil then
+            return ret
+          end
+        end
+      end
+    end,
     one_per = {
-      kitty = require("util.client").is_kitty(),
-      -- wezterm = require("util.client").is_wezterm(),
+      kitty = is_kitty,
+      wezterm = is_wezterm,
     },
   }
 end
