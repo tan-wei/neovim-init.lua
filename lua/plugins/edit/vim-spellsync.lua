f3db2d1883f6a42aff4acb07bbd19397e9ed78a8
Wei Tan <tanwei0404@163.com>
2024-02-18 12:50:59 +0800

    plugin: Add vim-spellsync

---
+++ lua/plugins/edit/vim-spellsync.lua
@@ -0,0 +1,12 @@
+local M = {
+  "micarmst/vim-spellsync",
+  event = "VeryLazy",
+}
+
+M.init = function()
+  vim.g.spellsync_run_at_startup = 1
+  vim.g.spellsync_enable_git_union_merge = 1
+  vim.g.spellsync_enable_git_ignore = 1
+end
+
+return M
