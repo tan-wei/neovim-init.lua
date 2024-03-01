9b44a79ad1ac5e33048864403836c40de7913923
Wei Tan <tanwei0404@163.com>
2024-03-01 21:06:29 +0800

    colorscheme: Add distinct-nvim

---
+++ lua/plugins/colorscheme/distinct-nvim.lua
@@ -0,0 +1,12 @@
+local M = {
+  "https://gitlab.com/bartekjaszczak/distinct-nvim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "distinct")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
