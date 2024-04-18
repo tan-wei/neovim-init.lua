baa34be03761c4df52817e64a14fb9b9b5dbd80f
Wei Tan <tanwei0404@163.com>
2024-04-18 16:46:15 +0800

    config: Disable prettier now

--- lua/plugins/lsp/none-ls.lua
+++ lua/plugins/lsp/none-ls.lua
@@ -14,7 +14,7 @@ M.config = function()
   null_ls.setup {
     debug = false,
     sources = {
-      formatting.prettier.with { extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } },
+      -- formatting.prettier.with { extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } },
       formatting.black.with { extra_args = { "--fast" } },
       formatting.stylua,
       -- diagnostics.flake8
