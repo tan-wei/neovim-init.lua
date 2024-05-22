43db8251e9d8a96286fa3725592ad321c8ff0ee0
Wei Tan <tanwei0404@163.com>
2024-05-22 09:46:48 +0800

    config: Configure for modes.nvim to play well with modicator.nvim

--- lua/plugins/ui/modes.lua
+++ lua/plugins/ui/modes.lua
@@ -5,7 +5,10 @@ local M  = {
 
 M.config = function()
   require('modes').setup({
-    ignore_filetypes = { 'NvimTree', 'TelescopePrompt' }
+    ignore_filetypes = { 'NvimTree', 'TelescopePrompt' },
+    set_number = false,
+    set_cursor = true,
+    set_cursorline = false,
   })
 end
 
