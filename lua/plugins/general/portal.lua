6c9382f727d4f19c91cd3a598fb7c80e14572ebe
Wei Tan <tanwei0404@163.com>
2024-06-12 10:52:28 +0800

    plugin: Add portal.nvim

---
+++ lua/plugins/general/portal.lua
@@ -0,0 +1,13 @@
+local M = {
+  "cbochs/portal.nvim",
+  dependencies = {
+    "cbochs/grapple.nvim",
+    "ThePrimeagen/harpoon"
+  },
+  cmd = { "Portal" },
+}
+
+-- TODO: This plugin should write more configurations
+M.config = true
+
+return M
