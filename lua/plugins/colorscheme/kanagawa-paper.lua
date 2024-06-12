5149feb6edb648f5f43239c408496891b28f8041
Wei Tan <tanwei0404@163.com>
2024-06-12 08:17:54 +0800

    colorscheme: Add kanagawa-paper.nvim

---
+++ lua/plugins/colorscheme/kanagawa-paper.lua
@@ -0,0 +1,14 @@
+local M = {
+  "sho-87/kanagawa-paper.nvim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "kanagawa-paper")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+M.config = true
+
+return M
