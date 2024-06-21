c8ec1b453f007792ac42aad98998186b710b55b5
Wei Tan <tanwei0404@163.com>
2024-06-21 17:35:50 +0800

    colorscheme: Use molokai.nvim instead of molokai

--- lua/plugins/colorscheme/molokai.lua
+++ lua/plugins/colorscheme/molokai.lua
@@ -1,12 +1,9 @@
 local M = {
-  "tomasr/molokai",
+  "UtkarshVerma/molokai.nvim",
   lazy = true,
 }
 
 M.init = function()
-  vim.g.molokai_original = 0
-  vim.g.rehash256 = 1
-
   local available_colorschemes = vim.g.available_colorschemes or {}
   table.insert(available_colorschemes, "molokai")
   vim.g.available_colorschemes = available_colorschemes
