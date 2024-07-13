ab47bc9e14ab36fad09a13b5ea1687be7552dc56
Wei Tan <tanwei0404@163.com>
2024-07-13 14:39:16 +0800

    colorscheme: Add setup opts for evergarden

--- lua/plugins/colorscheme/evergarden.lua
+++ lua/plugins/colorscheme/evergarden.lua
@@ -9,4 +9,18 @@ M.init = function()
   vim.g.available_colorschemes = available_colorschemes
 end
 
+M.opts = {
+  transparent_background = false,
+  contrast_dark = "medium", -- 'hard'|'medium'|'soft'
+  override_terminal = true,
+  style = {
+    tabline = { reverse = true, color = "green" },
+    search = { reverse = false, inc_reverse = true },
+    types = { italic = true },
+    keyword = { italic = true },
+    comment = { italic = true },
+  },
+  overrides = {}, -- add custom overrides
+}
+
 return M
