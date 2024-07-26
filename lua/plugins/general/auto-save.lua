d22708c726e085eed80df352d0fd57fc24a80b84
Wei Tan <tanwei0404@163.com>
2024-07-26 14:21:55 +0800

    config: Use a fork version auto-save.nvim

--- lua/plugins/general/auto-save.lua
+++ lua/plugins/general/auto-save.lua
@@ -1,21 +1,15 @@
 local M = {
-  "pocco81/auto-save.nvim",
+  "okuuva/auto-save.nvim",
   ft = {
     -- "markdown",
     "tex",
     "org",
     "txt",
   },
+  cmd = "ASToggle",
 }
 
 M.config = function()
-  local auto_save_fts = {
-    -- "markdown",
-    "tex",
-    "org",
-    "txt",
-  }
-
   require("auto-save").setup {
     enabled = true,
     execution_message = {
@@ -25,13 +19,16 @@ M.config = function()
       dim = 0.18,
       cleaning_interval = 1250,
     },
-    trigger_events = { "InsertLeave" },
-
+    trigger_events = {
+      immediate_save = { "BufLeave", "FocusLost" },
+      defer_save = { "InsertLeave", "TextChanged" },
+      cancel_defered_save = { "InsertEnter" },
+    },
     condition = function(buf)
       local fn = vim.fn
       local utils = require "auto-save.utils.data"
 
-      if fn.getbufvar(buf, "&modifiable") == 1 and utils.set_of(auto_save_fts)[fn.getbufvar(buf, "&filetype")] then
+      if fn.getbufvar(buf, "&modifiable") == 1 and utils.set_of(M.ft)[fn.getbufvar(buf, "&filetype")] then
         local undotree = fn.undotree()
         if undotree.seq_last ~= undotree.seq_cur then
           return false
@@ -43,6 +40,8 @@ M.config = function()
     end,
     write_all_buffers = false,
     debounce_delay = 135,
+    noautocmd = false,
+    lockmarks = false,
     callbacks = {
       enabling = nil,
       disabling = nil,
