fefc65b77664a24382792e98c442ddec469306e7
Wei Tan <tanwei0404@163.com>
2024-08-04 16:07:09 +0800

    plugin: Add vim-template

---
+++ lua/plugins/edit/vim-template.lua
@@ -0,0 +1,16 @@
+local M = {
+  "aperezdc/vim-template",
+  cmd = { "Template", "TemplateHere" },
+}
+
+M.init = function()
+  vim.g.templates_no_autocmd = 0
+  vim.g.templates_directory = {vim.fn.stdpath "config" .. "/templates/vim-template/"}
+  vim.g.templates_name_prefix = ".vim-template:"
+  vim.g.templates_global_name_prefix = "=template="
+  vim.g.templates_no_builtin_templates = 0
+  vim.g.templates_user_variables = {}
+  vim.g.templates_use_licensee = 0
+end
+
+return M
---
+++ templates/vim-template/=template=.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#ifndef %GUARD%
+#define %GUARD%
+
+%HERE%
+
+#endif /* !%GUARD% */
