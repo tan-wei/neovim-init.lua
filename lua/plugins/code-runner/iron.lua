a5a3d79601aed47039b160123bdfac182edb50e0
Wei Tan <tanwei0404@163.com>
2024-08-24 22:13:14 +0800

    config: Remove keymaps in setup function

--- lua/plugins/code-runner/iron.lua
+++ lua/plugins/code-runner/iron.lua
@@ -18,25 +18,24 @@ M.config = function()
       repl_open_cmd = require("iron.view").split.vertical "40%",
     },
     keymaps = {
-      -- TODO: Mark related keymap should be configured
-      -- send_motion = "<space>rs",
-      -- visual_send = "<space>rs",
-      -- send_file = "<space>rf",
-      -- send_line = "<space>rl",
-      -- send_until_cursor = "<space>ru",
-      -- send_mark = "<space>sm",
-      -- mark_motion = "<space>mc",
-      -- mark_visual = "<space>mc",
-      -- remove_mark = "<space>md",
-      cr = "<space>s<cr>",
-      interrupt = "<space>s<space>",
-      exit = "<space>sq",
-      clear = "<space>cl",
+      send_motion = "",
+      visual_send = "",
+      send_file = "",
+      send_line = "",
+      send_until_cursor = "",
+      send_mark = "",
+      mark_motion = "",
+      mark_visual = "",
+      remove_mark = "",
+      cr = "",
+      interrupt = "",
+      exit = "",
+      clear = "",
     },
     highlight = {
       italic = true,
     },
-    ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+    ignore_blank_lines = true,
   }
 end
 
