3f145b4d0d31d287528b718d0608286781cbbf99
Wei Tan <tanwei0404@163.com>
2024-08-28 09:23:29 +0800

    config: Configure for nvim-ts-autotag

--- lua/plugins/treesitter/nvim-ts-autotag.lua
+++ lua/plugins/treesitter/nvim-ts-autotag.lua
@@ -6,4 +6,14 @@ local M = {
   event = "VeryLazy",
 }
 
+M.config = function()
+  require("nvim-ts-autotag").setup {
+    opts = {
+      enable_close = true, -- Auto close tags
+      enable_rename = true, -- Auto rename pairs of tags
+      enable_close_on_slash = false, -- Auto close on trailing </
+    },
+  }
+end
+
 return M
