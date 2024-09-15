3379483e8111f2abd3676878855e49dcb52a330c
Wei Tan <tanwei0404@163.com>
2024-09-15 12:11:19 +0800

    plugin: Add hexmode

---
+++ lua/plugins/edit/hexmode.lua
@@ -0,0 +1,11 @@
+local M = {
+  "fidian/hexmode",
+  cmd = "Hexmode",
+}
+
+M.init = function()
+  -- vim.g.hexmode_patterns = "*.bin,*.exe,*.dat,*.o"
+  -- vim.g.hexmode_xxd_options = "-g 1"
+end
+
+return M
