90668caac6676a0da83205389aa9b785b01abbf9
Wei Tan <tanwei0404@163.com>
2024-09-24 23:14:54 +0800

    colorscheme: Add obscure.nvim

---
+++ lua/plugins/colorscheme/obscure.lua
@@ -0,0 +1,14 @@
+local M = {
+  "killitar/obscure.nvim",
+  lazy = true,
+}
+
+M.config = true
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "obscure")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
