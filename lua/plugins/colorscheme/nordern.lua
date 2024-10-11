be1628c9bd8a6e2afc5c4ebed38ddc631c2fbf29
Wei Tan <tanwei0404@163.com>
2024-10-11 14:58:39 +0800

    colorscheme: Fix typo

--- lua/plugins/colorscheme/nordern.lua
+++ lua/plugins/colorscheme/nordern.lua
@@ -5,7 +5,7 @@ local M = {
 
 M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
-  table.insert(available_colorschemes, "nodern")
+  table.insert(available_colorschemes, "nordern")
   vim.g.available_colorschemes = available_colorschemes
 end
 
