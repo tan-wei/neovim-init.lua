7ea5a55120929454fecac150e83ca2b98d174f36
Wei Tan <tanwei0404@163.com>
2024-10-16 22:41:44 +0800

    colorscheme: Add luma-nvim

---
+++ lua/plugins/colorscheme/luma-nvim.lua
@@ -0,0 +1,19 @@
+local M = {
+  "https://gitlab.com/bartekjaszczak/luma-nvim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "luma")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+M.config = function()
+  require("luma").setup {
+    theme = "dark",
+    contrast = "medium",
+  }
+end
+
+return M
