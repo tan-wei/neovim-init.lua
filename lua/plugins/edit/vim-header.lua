55c608e79b7a843b6acc7617469ca0df71e3f33f
Wei Tan <tanwei0404@163.com>
2024-10-22 10:47:43 +0800

    config: Make vim-header not lazy loading

--- lua/plugins/edit/vim-header.lua
+++ lua/plugins/edit/vim-header.lua
@@ -1,11 +1,10 @@
 local M = {
   "alpertuna/vim-header",
-  cmd = { "AddHeader", "AddMinHeader" },
 }
 
 M.init = function()
   vim.g.header_auto_add_header = 0
-  vim.g.header_auto_update_header = 0
+  vim.g.header_auto_update_header = 1
   vim.g.header_field_filename = 1
   vim.g.header_field_project = "TODO: Project Name"
   vim.g.header_field_author = "Winterreise"
