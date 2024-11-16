0cffd2cafb19faf76c7534816dbf281515b39d95
Wei Tan <tanwei0404@163.com>
2024-11-16 20:45:09 +0800

    config: Just disable all key mappings for mkdnflow.nvim

--- lua/plugins/markdown/mkdnflow.lua
+++ lua/plugins/markdown/mkdnflow.lua
@@ -7,6 +7,44 @@ local M = {
 }
 
 -- TODO: This plugin should write more configurations
-M.opts = {}
+M.opts = {
+  mappings = {
+    MkdnEnter = false,
+    MkdnTab = false,
+    MkdnSTab = false,
+    MkdnNextLink = false,
+    MkdnPrevLink = false,
+    MkdnNextHeading = false,
+    MkdnPrevHeading = false,
+    MkdnGoBack = false,
+    MkdnGoForward = false,
+    MkdnCreateLink = false,
+    MkdnCreateLinkFromClipboard = false,
+    MkdnFollowLink = false,
+    MkdnDestroyLink = false,
+    MkdnTagSpan = false,
+    MkdnMoveSource = false,
+    MkdnYankAnchorLink = false,
+    MkdnYankFileAnchorLink = false,
+    MkdnIncreaseHeading = false,
+    MkdnDecreaseHeading = false,
+    MkdnToggleToDo = false,
+    MkdnNewListItem = false,
+    MkdnNewListItemBelowInsert = false,
+    MkdnNewListItemAboveInsert = false,
+    MkdnExtendList = false,
+    MkdnUpdateNumbering = false,
+    MkdnTableNextCell = false,
+    MkdnTablePrevCell = false,
+    MkdnTableNextRow = false,
+    MkdnTablePrevRow = false,
+    MkdnTableNewRowBelow = false,
+    MkdnTableNewRowAbove = false,
+    MkdnTableNewColAfter = false,
+    MkdnTableNewColBefore = false,
+    MkdnFoldSection = false,
+    MkdnUnfoldSection = false,
+  },
+}
 
 return M
