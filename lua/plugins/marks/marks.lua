3ae987ce58adeccacdcfb84f194817b84a20a810
Wei Tan <tanwei0404@163.com>
2024-11-17 23:13:15 +0800

    plugin: Add marks.nvim

---
+++ lua/plugins/marks/marks.lua
@@ -0,0 +1,23 @@
+local M = {
+  "chentoast/marks.nvim",
+  event = "VeryLazy",
+}
+
+M.opts = {
+  default_mappings = true,
+  builtin_marks = { ".", "<", ">", "^" },
+  cyclic = true,
+  force_write_shada = false,
+  refresh_interval = 250,
+  sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
+  excluded_filetypes = {},
+  excluded_buftypes = {},
+  bookmark_0 = {
+    sign = "⚑",
+    virt_text = "Bookmark",
+    annotate = true,
+  },
+  mappings = {},
+}
+
+return M
