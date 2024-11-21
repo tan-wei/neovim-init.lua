7a12cacae3947957f141c1b558f0c9856e6378fa
Wei Tan <tanwei0404@163.com>
2024-11-21 15:04:56 +0800

    plugin: Disable pretty-fold.nvim due to it is out of date

--- lua/plugins/indent/pretty-fold.lua
+++ lua/plugins/indent/pretty-fold.lua
@@ -1,6 +1,7 @@
 local M = {
   "anuvyklack/pretty-fold.nvim",
   event = "VeryLazy",
+  enabled = false, -- Now luajit is update, it will cause neovim crash
 }
 
 -- TODO: This plugin should write more configurations
