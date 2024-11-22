769c3a4d70613e800d5823fbd13e5b522d924d3f
Wei Tan <tanwei0404@163.com>
2024-11-22 22:40:01 +0800

    plugin: Use a fork version for pretty-fold.nvim

--- lua/plugins/indent/pretty-fold.lua
+++ lua/plugins/indent/pretty-fold.lua
@@ -1,7 +1,7 @@
 local M = {
-  "anuvyklack/pretty-fold.nvim",
+  -- "anuvyklack/pretty-fold.nvim",
+  "bbjornstad/pretty-fold.nvim", -- Fork to fix #38
   event = "VeryLazy",
-  enabled = false, -- Now luajit is update, it will cause neovim crash
 }
 
 -- TODO: This plugin should write more configurations
