f27a28b3ea05d0a6864ae72cf3f34c5db6f17c59
Wei Tan <tanwei0404@163.com>
2024-12-09 09:23:21 +0800

    lsp: Fix configure for ccls

--- lua/user/lsp/settings/ccls.lua
+++ lua/user/lsp/settings/ccls.lua
@@ -1,14 +1,14 @@
 local util = require "lspconfig.util"
 
 return {
-  root_dir = [[ util.root_pattern("compile_commands.json", "compile_flags.txt", "CMakeLists.txt", "Makefile", ".git") or util.path.dirname ]],
+  root_dir =  util.root_pattern("compile_commands.json", "compile_flags.txt", "CMakeLists.txt", "Makefile", ".git") or util.path.dirname,
   init_options = {
     compilationDatabaseDirectory = "build",
     index = {
       threads = 2,
     },
     cache = {
-      directory = ".vscode/.ccls-cache",
+      directory = ".nvim/.ccls-cache",
     },
   },
 }
