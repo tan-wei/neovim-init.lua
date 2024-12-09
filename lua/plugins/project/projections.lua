4a81d4618c80171f1d9f26fc9d831a3e70694e9c
Wei Tan <tanwei0404@163.com>
2024-12-09 17:15:54 +0800

    plugin: Use projections.nvim pre_release branch

--- lua/plugins/project/projections.lua
+++ lua/plugins/project/projections.lua
@@ -1,5 +1,9 @@
 local M = {
   "GnikDroy/projections.nvim",
+  branch = "pre_release",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+  },
   event = "VeryLazy",
 }
 
@@ -7,6 +11,8 @@ M.config = function()
   require("projections").setup {
     patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
   }
+
+  require("telescope").load_extension "projections"
 end
 
 return M
