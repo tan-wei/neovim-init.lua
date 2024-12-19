baf01bb6fb16d58024a9fc20b5b28991a4ab6ec6
Wei Tan <tanwei0404@163.com>
2024-12-19 15:40:19 +0800

    lsp: Use root directory to save cache files

--- lua/user/lsp/settings/ccls.lua
+++ lua/user/lsp/settings/ccls.lua
@@ -1,15 +1,17 @@
 local util = require "lspconfig.util"
 
+local root_dir =
+  vim.fs.dirname(vim.fs.find({ "compile_commands.json", "compile_flags.txt", ".git" }, { upward = true })[1])
+
 return {
-  root_dir = util.root_pattern("compile_commands.json", "compile_flags.txt", "CMakeLists.txt", "Makefile", ".git")
-    or util.path.dirname,
+  root_dir = root_dir or util.path.dirname,
   init_options = {
     compilationDatabaseDirectory = "build",
     index = {
       threads = 2,
     },
     cache = {
-      directory = ".nvim/.ccls-cache",
+      directory = vim.fs.normalize(root_dir .. "/.nvim/.ccls-cache"),
     },
   },
 }
