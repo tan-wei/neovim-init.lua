cc26ca047428aabf123ef7e4d08c3f216d3ce911
Wei Tan <tanwei0404@163.com>
2024-12-19 18:33:59 +0800

    lsp: Fix for not found root for ccls

--- lua/user/lsp/settings/ccls.lua
+++ lua/user/lsp/settings/ccls.lua
@@ -3,6 +3,18 @@ local util = require "lspconfig.util"
 local root_dir =
   vim.fs.dirname(vim.fs.find({ "compile_commands.json", "compile_flags.txt", ".git" }, { upward = true })[1])
 
+local cache_directory = nil
+
+if root_dir then
+  if type(root_dir) == "function" then
+    if not root_dir() then
+      cache_directory = vim.fs.normalize(root_dir() .. "/.nvim/.ccls-cache")
+    end
+  else
+    cache_directory = vim.fs.normalize(root_dir .. "/.nvim/.ccls-cache")
+  end
+end
+
 return {
   root_dir = root_dir or util.path.dirname,
   init_options = {
@@ -11,7 +23,7 @@ return {
       threads = 2,
     },
     cache = {
-      directory = vim.fs.normalize(root_dir .. "/.nvim/.ccls-cache"),
+      directory = cache_directory,
     },
   },
 }
