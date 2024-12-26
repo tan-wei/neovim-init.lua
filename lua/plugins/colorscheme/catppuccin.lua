7a2e68274043570ba44149f61d722d63dc9e7e9f
Wei Tan <tanwei0404@163.com>
2024-12-26 08:32:55 +0800

    colorscheme: Reconfigure cappuccin, add more colorschemes

--- lua/plugins/colorscheme/catppuccin.lua
+++ lua/plugins/colorscheme/catppuccin.lua
@@ -7,7 +7,13 @@ local M = {
 M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
   table.insert(available_colorschemes, "catppuccin")
+  table.insert(available_colorschemes, "catppuccin-latte")
+  table.insert(available_colorschemes, "catppuccin-frappe")
+  table.insert(available_colorschemes, "catppuccin-macchiato")
+  table.insert(available_colorschemes, "catppuccin-mocha")
   vim.g.available_colorschemes = available_colorschemes
 end
+
+M.config = true
 
 return M
