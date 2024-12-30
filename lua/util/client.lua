8258d6afc4264e239b01ac74820a65d1dbe29798
Wei Tan <tanwei0404@163.com>
2024-12-30 15:52:26 +0800

    config: More CUI check

--- lua/util/client.lua
+++ lua/util/client.lua
@@ -32,12 +32,38 @@ M.is_wezterm = function()
   return vim.env.TERM == "wezterm"
 end
 
+M.is_ghostty = function()
+  return vim.env.TERM == "xterm-ghostty"
+end
+
+M.is_alacritty = function()
+  return vim.env.TERM == "alacritty"
+end
+
+M.is_windows_terminal = function()
+  if vim.fn.has "win32" == 1 then
+    local wt_session = os.getenv "WT_SESSION"
+    if wt_session ~= nil then
+      return true
+    else
+      return false
+    end
+  else
+    return false
+  end
+end
+
 M.is_other_terminal = function()
   return vim.env.TERM == "xterm-256color"
 end
 
 M.is_cui_client = function()
-  return M.is_other_terminal() or M.is_wezterm() or M.is_kitty()
+  return M.is_other_terminal()
+    or M.is_wezterm()
+    or M.is_kitty()
+    or M.is_ghostty()
+    or M.is_alacritty()
+    or M.is_windows_terminal()
 end
 
 return M
