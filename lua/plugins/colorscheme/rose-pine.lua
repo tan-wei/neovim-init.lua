2dd2a0cb9a40060a3fc2a794f732804cabaa2888
Wei Tan <tanwei0404@163.com>
2024-12-31 09:21:46 +0800

    config: Add more colorschemes for rose-pine

--- lua/plugins/colorscheme/rose-pine.lua
+++ lua/plugins/colorscheme/rose-pine.lua
@@ -8,6 +8,7 @@ M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
   table.insert(available_colorschemes, "rose-pine")
   table.insert(available_colorschemes, "rose-pine-main")
+  table.insert(available_colorschemes, "rose-pine-moon")
   vim.g.available_colorschemes = available_colorschemes
 end
 
