be1a2cf9fae0a85b8b9fdb5dfbdbff7f1a75f617
Wei Tan <tanwei0404@163.com>
2024-12-31 09:18:51 +0800

    config: Add more colorschemes for kanagawa.nvim

--- lua/plugins/colorscheme/kanagawa.lua
+++ lua/plugins/colorscheme/kanagawa.lua
@@ -6,6 +6,8 @@ local M = {
 M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
   table.insert(available_colorschemes, "kanagawa")
+  table.insert(available_colorschemes, "kanagawa-wave")
+  table.insert(available_colorschemes, "kanagawa-dragon")
   vim.g.available_colorschemes = available_colorschemes
 end
 
