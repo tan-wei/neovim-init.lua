6740b4f2bd1a3b18873199de920673f37cb15da1
Wei Tan <tanwei0404@163.com>
2025-01-05 19:45:47 +0800

    config: Ensure install black

--- lua/plugins/lsp/mason-tool-installer.lua
+++ lua/plugins/lsp/mason-tool-installer.lua
@@ -1,11 +1,12 @@
 local M = {
   "WhoIsSethDaniel/mason-tool-installer.nvim",
-  cmd = { "MasonToolsInstall", "MasonToolsUpdate", "MasonToolsClean" },
 }
 
 M.config = function()
   require("mason-tool-installer").setup {
-    ensure_installed = {},
+    ensure_installed = {
+      "black",
+    },
     auto_update = true,
     start_delay = 3000,
     debounce_hours = 5,
