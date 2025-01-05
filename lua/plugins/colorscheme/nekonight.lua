498c601d397af5a73760bebcd8abad6802e3b8ac
Wei Tan <tanwei0404@163.com>
2025-01-05 17:27:55 +0800

    fix: Fix typo

--- lua/plugins/colorscheme/nekonight.lua
+++ lua/plugins/colorscheme/nekonight.lua
@@ -7,7 +7,7 @@ M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
   table.insert(available_colorschemes, "nekonight")
   table.insert(available_colorschemes, "nekonight-night")
-  table.insert(available_colorschemes, "nekonight-store")
+  table.insert(available_colorschemes, "nekonight-storm")
   table.insert(available_colorschemes, "nekonight-moon")
   vim.g.available_colorschemes = available_colorschemes
 end
