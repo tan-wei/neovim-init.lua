3f7f98e281a7252866756ce26e5ae2a9a4d99039
Wei Tan <tanwei0404@163.com>
2025-01-08 16:25:41 +0800

    lsp: Fix for not found root for ccls when change project directory

--- lua/user/lsp/settings/ccls.lua
+++ lua/user/lsp/settings/ccls.lua
@@ -1,20 +1,3 @@
-local util = require "lspconfig.util"
-
-local root_dir =
-  vim.fs.dirname(vim.fs.find({ "compile_commands.json", "compile_flags.txt", ".git" }, { upward = true })[1])
-
-local cache_directory = nil
-
-if root_dir then
-  if type(root_dir) == "function" then
-    if not root_dir() then
-      cache_directory = vim.fs.normalize(root_dir() .. "/.nvim/.ccls-cache")
-    end
-  else
-    cache_directory = vim.fs.normalize(root_dir .. "/.nvim/.ccls-cache")
-  end
-end
-
 return {
   root_dir = root_dir or util.path.dirname,
   init_options = {
@@ -23,7 +6,7 @@ return {
       threads = 2,
     },
     cache = {
-      directory = cache_directory,
+      directory = vim.fn.stdpath "cache" .. "/.ccls-cache",
     },
   },
 }
