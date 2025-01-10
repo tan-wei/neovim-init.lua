1366683257461e457db4f424b214dd2db09e5054
Wei Tan <tanwei0404@163.com>
2025-01-10 09:16:55 +0800

    colorscheme: Remove light colorscheme

--- lua/plugins/colorscheme/nekonight.lua
+++ lua/plugins/colorscheme/nekonight.lua
@@ -5,7 +5,6 @@ local M = {
 
 M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
-  table.insert(available_colorschemes, "nekonight")
   table.insert(available_colorschemes, "nekonight-night")
   table.insert(available_colorschemes, "nekonight-storm")
   table.insert(available_colorschemes, "nekonight-moon")
