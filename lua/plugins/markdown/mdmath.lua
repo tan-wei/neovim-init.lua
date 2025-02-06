02151e566b49d8e6607b95aa07d01a7e383f78da
Wei Tan <tanwei0404@163.com>
2025-02-06 10:45:53 +0800

    plugin: Add mdmath.nvim

---
+++ lua/plugins/markdown/mdmath.lua
@@ -0,0 +1,14 @@
+local M = {
+  "Thiago4532/mdmath.nvim",
+  enabled = require("util.os").is_linux() or require("util.os").is_macos(),
+  cond = require("util.client").is_kitty(),
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  },
+  ft = { "markdown" },
+  cmd = { "MdMath" },
+}
+
+M.opts = {}
+
+return M
