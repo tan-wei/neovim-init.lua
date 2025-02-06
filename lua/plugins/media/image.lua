aa4a8c701a441018e177fde742ccda48331a806b
Wei Tan <tanwei0404@163.com>
2025-02-06 10:35:53 +0800

    plugin: Add image.nvim

---
+++ lua/plugins/media/image.lua
@@ -0,0 +1,12 @@
+local M = {
+  "3rd/image.nvim",
+  enabled = require("util.os").is_linux() or require("util.os").is_macos(),
+  cond = require("util.client").is_kitty(),
+  rocks = {
+    hererocks = true,
+  },
+}
+
+M.opts = {}
+
+return M
