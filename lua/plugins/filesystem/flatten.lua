4b7a4c9242b6eaab9ace5c33bd7566d1492a4939
Wei Tan <tanwei0404@163.com>
2025-02-11 12:16:05 +0800

    config: Configure for toggleterm in flatten.nvim

--- lua/plugins/filesystem/flatten.lua
+++ lua/plugins/filesystem/flatten.lua
@@ -48,6 +48,46 @@ M.config = function()
         end
       end
     end,
+    window = {
+      open = "alternate",
+    },
+    hooks = {
+      should_block = function(argv)
+        return vim.tbl_contains(argv, "-b")
+      end,
+      pre_open = function()
+        local term = require "toggleterm.terminal"
+        local termid = term.get_focused_id()
+        saved_terminal = term.get(termid)
+      end,
+      post_open = function(bufnr, winnr, ft, is_blocking)
+        if is_blocking and saved_terminal then
+          saved_terminal:close()
+        else
+          vim.api.nvim_set_current_win(winnr)
+
+          require("wezterm").switch_pane.id(tonumber(os.getenv "WEZTERM_PANE"))
+        end
+
+        if ft == "gitcommit" or ft == "gitrebase" then
+          vim.api.nvim_create_autocmd("BufWritePost", {
+            buffer = bufnr,
+            once = true,
+            callback = vim.schedule_wrap(function()
+              vim.api.nvim_buf_delete(bufnr, {})
+            end),
+          })
+        end
+      end,
+      block_end = function()
+        vim.schedule(function()
+          if saved_terminal then
+            saved_terminal:open()
+            saved_terminal = nil
+          end
+        end)
+      end,
+    },
     integrations = {
       kitty = is_kitty,
       wezterm = is_wezterm,
