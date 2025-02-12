8c93f7ad689804fa74ee1e11b8b01e1cf8702c5a
Wei Tan <tanwei0404@163.com>
2025-02-12 10:17:38 +0800

    config: Configure for nvim-neovlip.lua to filter out empty yanky

--- lua/plugins/register/nvim-neoclip.lua
+++ lua/plugins/register/nvim-neoclip.lua
@@ -7,9 +7,24 @@ local M = {
   event = "VeryLazy",
 }
 
--- TODO: This plugin should write more configurations
 M.config = function()
-  require("neoclip").setup()
+  local function is_whitespace(line)
+    return vim.fn.match(line, [[^\s*$]]) ~= -1
+  end
+
+  local function all(tbl, check)
+    for _, entry in ipairs(tbl) do
+      if not check(entry) then
+        return false
+      end
+    end
+    return true
+  end
+  require("neoclip").setup {
+    filter = function(data)
+      return not all(data.event.regcontents, is_whitespace)
+    end,
+  }
   require("telescope").load_extension "neoclip"
 end
 
