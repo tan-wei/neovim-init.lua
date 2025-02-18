9361063ef00c554ab486fda5a03057881e709eb6
Wei Tan <tanwei0404@163.com>
2025-02-18 10:21:41 +0800

    colorscheme: Now vn-night.nvim is fixed

--- lua/plugins/colorscheme/vn-night.lua
+++ lua/plugins/colorscheme/vn-night.lua
@@ -6,7 +6,7 @@ local M = {
 M.init = function()
   vim.g.one_allow_italics = 1
   local available_colorschemes = vim.g.available_colorschemes or {}
-  -- table.insert(available_colorschemes, "vn-night") -- buggy now
+  table.insert(available_colorschemes, "vn-night")
   vim.g.available_colorschemes = available_colorschemes
 end
 
