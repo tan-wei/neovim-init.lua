5bc53739c49b4d89d37a5ef7cda3b240cd576982
Wei Tan <tanwei0404@163.com>
2025-02-18 10:46:40 +0800

    colorscheme: Just disable one_monokai because of buggy

--- lua/plugins/colorscheme/one_monokai.lua
+++ lua/plugins/colorscheme/one_monokai.lua
@@ -7,7 +7,7 @@ M.init = function()
   vim.g.onedark_termcolors = 256
 
   local available_colorschemes = vim.g.available_colorschemes or {}
-  table.insert(available_colorschemes, "one_monokai")
+  -- table.insert(available_colorschemes, "one_monokai")
   vim.g.available_colorschemes = available_colorschemes
 end
 
