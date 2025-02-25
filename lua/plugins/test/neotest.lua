8734f2c6685b900b6036997193c957122a1f64b6
Wei Tan <tanwei0404@163.com>
2025-02-25 11:56:29 +0800

    config: Make fail test messages with virtual text

--- lua/plugins/test/neotest.lua
+++ lua/plugins/test/neotest.lua
@@ -14,11 +14,24 @@ local M = {
 }
 
 M.config = function()
+  local neotest_ns = vim.api.nvim_create_namespace "neotest"
+  vim.diagnostic.config({
+    virtual_text = {
+      format = function(diagnostic)
+        -- Convert newlines, tabs and whitespaces into a single whitespace
+        -- for improved virtual text readability
+        local message = diagnostic.message:gsub("[\r\n\t%s]+", " ")
+        return message
+      end,
+    },
+  }, neotest_ns)
+
   require("neotest").setup {
     adapters = {
       require "neotest-rust" { allow_file_types = { "rust" } },
       require("neotest-ctest").setup {
         is_test_file = function(file)
+          -- File name begins or ends with test[s]
           return string.find(file, "[tT][eE][sS][tT][sS]?_") ~= nil
             or string.find(file, "_[tT][eE][sS][tT][sS]?") ~= nil
         end,
