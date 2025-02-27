87a7983ac32144aa64646ddeca8bf47e7c547b5d
Wei Tan <tanwei0404@163.com>
2025-02-27 14:21:14 +0800

    keymap: Diable harpoon keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -115,33 +115,33 @@ M.setup = function()
 
   if status_ok then
     -- TODO: Should be change to our own keymaps
-    keymap("n", "<leader>a", function()
-      harpoon:list():add()
-    end)
-    keymap("n", "<C-e>", function()
-      harpoon.ui:toggle_quick_menu(harpoon:list())
-    end)
-
-    keymap("n", "<C-h>", function()
-      harpoon:list():select(1)
-    end)
-    keymap("n", "<C-t>", function()
-      harpoon:list():select(2)
-    end)
-    keymap("n", "<C-n>", function()
-      harpoon:list():select(3)
-    end)
-    keymap("n", "<C-s>", function()
-      harpoon:list():select(4)
-    end)
-
-    -- Toggle previous & next buffers stored within Harpoon list
-    keymap("n", "<C-S-P>", function()
-      harpoon:list():prev()
-    end)
-    keymap("n", "<C-S-N>", function()
-      harpoon:list():next()
-    end)
+    -- keymap("n", "<leader>a", function()
+    --   harpoon:list():add()
+    -- end)
+    -- keymap("n", "<C-e>", function()
+    --   harpoon.ui:toggle_quick_menu(harpoon:list())
+    -- end)
+    --
+    -- keymap("n", "<C-h>", function()
+    --   harpoon:list():select(1)
+    -- end)
+    -- keymap("n", "<C-t>", function()
+    --   harpoon:list():select(2)
+    -- end)
+    -- keymap("n", "<C-n>", function()
+    --   harpoon:list():select(3)
+    -- end)
+    -- keymap("n", "<C-s>", function()
+    --   harpoon:list():select(4)
+    -- end)
+    --
+    -- -- Toggle previous & next buffers stored within Harpoon list
+    -- keymap("n", "<C-S-P>", function()
+    --   harpoon:list():prev()
+    -- end)
+    -- keymap("n", "<C-S-N>", function()
+    --   harpoon:list():next()
+    -- end)
   end
 
   -- dial.nvim
