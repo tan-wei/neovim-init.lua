7fe7e6a5ce1d3b6ccb47c074ed73c4c655183000
Wei Tan <tanwei0404@163.com>
2025-03-23 17:18:07 +0800

    plugin: Add obsidian.nvim

---
+++ lua/plugins/markdown/obsidian.lua
@@ -0,0 +1,39 @@
+local M = {
+  "epwalsh/obsidian.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  ft = "markdown",
+}
+
+M.opts = {
+  workspaces = {
+    {
+      name = "buf-parent",
+      path = function()
+        return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+      end,
+    },
+    {
+      name = "no-vault",
+      path = function()
+        -- alternatively use the CWD:
+        -- return assert(vim.fn.getcwd())
+        return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+      end,
+      overrides = {
+        notes_subdir = vim.NIL, -- have to use 'vim.NIL' instead of 'nil'
+        new_notes_location = "current_dir",
+        templates = {
+          folder = vim.NIL,
+        },
+        disable_frontmatter = true,
+      },
+    },
+  },
+  ui = {
+    enable = false, -- NOTE: Enable it requires conceallevel set 1 or 2
+  },
+}
+
+return M
