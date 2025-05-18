5136ef34f054c670e347a647f69e17dfedfa6751
Wei Tan <tanwei0404@163.com>
2025-05-18 21:46:50 +0800

    config: De-duplicate items when go to definition

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -50,6 +50,31 @@ M.setup = function()
   vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
     border = "rounded",
   })
+
+  -- Credit: https://github.com/benlubas/.dotfiles/blob/main/nvim%2Flua%2Fbenlubas%2Flsp_handlers.lua
+  vim.lsp.handlers["textDocument/definition"] = function(_, result, ctx)
+    if not result or vim.tbl_isempty(result) then
+      vim.notify("[lsp]: Could not find definition", vim.log.levels.INFO)
+      return
+    end
+    local client = vim.lsp.get_client_by_id(ctx.client_id)
+    if not client then
+      return
+    end
+
+    if vim.islist(result) then
+      local results = vim.lsp.util.locations_to_items(result, client.offset_encoding)
+      local lnum, filename = results[1].lnum, results[1].filename
+      for _, val in pairs(results) do
+        if val.lnum ~= lnum or val.filename ~= filename then
+          return require("telescope.builtin").lsp_definitions()
+        end
+      end
+      vim.lsp.util.jump_to_location(result[1], client.offset_encoding, false)
+    else
+      vim.lsp.util.jump_to_location(result, client.offset_encoding, false)
+    end
+  end
 end
 
 local function lsp_keymaps(bufnr)
