f3fd58d03cec1ebec6ec8fd55e759eac17120b09
Wei Tan <tanwei0404@163.com>
2025-05-28 08:19:53 +0800

    config: Update due to latest version change configure structure

--- lua/plugins/ui/modes.lua
+++ lua/plugins/ui/modes.lua
@@ -5,7 +5,7 @@ local M = {
 
 M.config = function()
   require("modes").setup {
-    ignore_filetypes = { "NvimTree", "TelescopePrompt" },
+    ignore = { "NvimTree", "TelescopePrompt" },
     set_number = false,
     set_cursor = true,
     set_cursorline = false,
