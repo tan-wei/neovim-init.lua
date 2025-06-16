a54ac9e014694694c74a57d1db7e2c0d9bbcd1c9
Wei Tan <tanwei0404@163.com>
2025-06-16 22:09:15 +0800

    colorscheme: Use flexoki-neovim instead

--- lua/plugins/colorscheme/flexoki.lua
+++ lua/plugins/colorscheme/flexoki.lua
@@ -1,12 +1,12 @@
 local M = {
-  "stevedylandev/flexoki-nvim",
+  "kepano/flexoki-neovim",
   name = "flexoki",
   lazy = true,
 }
 
 M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
-  table.insert(available_colorschemes, "flexoki")
+  table.insert(available_colorschemes, "flexoki-dark")
   vim.g.available_colorschemes = available_colorschemes
 end
 
