df0d1d4c1f750b3746281ea83ee88c3c89e5d5fa
Wei Tan <tanwei0404@163.com>
2025-06-25 18:40:03 +0800

    config: Make vim-matchup work like sentiment.nvim

--- lua/plugins/edit/vim-markup.lua
+++ lua/plugins/edit/vim-markup.lua
@@ -9,6 +9,8 @@ local M = {
 M.init = function()
   vim.g.loaded_matchit = 1
   vim.g.matchup_matchparen_offscreen = { method = "popup" }
+  vim.g.matchup_matchparen_deferred = 1
+  vim.g.matchup_matchparen_hi_surround_always = 1
 end
 
 return M
