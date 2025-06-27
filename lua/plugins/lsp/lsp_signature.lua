56621ef651a262e814b007d9bc17ef22c52058d1
Wei Tan <tanwei0404@163.com>
2025-06-27 16:18:07 +0800

    config: Disable error message for some cases

--- lua/plugins/lsp/lsp_signature.lua
+++ lua/plugins/lsp/lsp_signature.lua
@@ -10,6 +10,16 @@ M.config = function()
     handler_opts = { border = "rounded" },
     max_width = 80,
     noice = false, -- TODO: Will cause error, and the author consider to deprecate this option
+    ignore_error = function(err, ctx, config)
+      local client = vim.lsp.get_client_by_id(ctx.client_id)
+      if client and vim.tbl_contains({ "rust_analyer", "clangd", "ccls" }, client.name) then
+        return true
+      end
+
+      if vim.tbl_contains({}, err.code_name) then
+        return true
+      end
+    end,
   }
 end
 
