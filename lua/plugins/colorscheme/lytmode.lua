c964c4cb10a1fbfa1fb7a2339a615c990fd05bb5
Wei Tan <tanwei0404@163.com>
2025-07-11 16:20:45 +0800

    colorscheme: Add lytmode

---
+++ lua/plugins/colorscheme/lytmode.lua
@@ -0,0 +1,16 @@
+local M = {
+  "github-main-user/lytmode.nvim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "lytmode")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+M. config = function()
+  require('lytmode').setup()
+end
+
+return M
