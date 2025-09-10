565ea5e4b836e753421558be029de9ac2e564f2d
Wei Tan <tanwei0404@163.com>
2025-09-10 13:15:21 +0800

    config: Make raimbow_csv work

--- lua/plugins/csv/csv.lua
+++ lua/plugins/csv/csv.lua
@@ -1,5 +1,11 @@
 local M = {
   "chrisbra/csv.vim",
+  ft = { "csv" },
 }
+
+M.config = function()
+  vim.g.csv_no_column_highlight = 1
+  vim.g.csv_highlight_column = 0
+end
 
 return M
--- lua/plugins/csv/rainbow_csv.lua
+++ lua/plugins/csv/rainbow_csv.lua
@@ -3,4 +3,19 @@ local M = {
   ft = "csv",
 }
 
+M.config = function()
+  vim.g.rcsv_colorpairs = {
+    { "red", "red" },
+    { "blue", "blue" },
+    { "green", "green" },
+    { "magenta", "magenta" },
+    { "NONE", "NONE" },
+    { "darkred", "darkred" },
+    { "darkblue", "darkblue" },
+    { "darkgreen", "darkgreen" },
+    { "darkmagenta", "darkmagenta" },
+    { "darkcyan", "darkcyan" },
+  }
+end
+
 return M
