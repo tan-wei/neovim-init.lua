d86c9c108a9a38f9389d19dac5725ba1595afca8
Wei Tan <tanwei0404@163.com>
2025-10-03 21:06:02 +0800

    colorscheme: Add rusty

---
+++ lua/plugins/colorscheme/rusty.lua
@@ -0,0 +1,14 @@
+local M = {
+  "armannikoyan/rusty",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "rusty")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+M.opts = {}
+
+return M
