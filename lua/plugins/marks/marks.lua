ea979de6bf7ee04a45a1209e5a021011df0c8113
Wei Tan <tanwei0404@163.com>
2025-10-03 23:13:18 +0800

    config: Add ignore lists for marks.nvim

--- lua/plugins/marks/marks.lua
+++ lua/plugins/marks/marks.lua
@@ -10,8 +10,14 @@ M.opts = {
   force_write_shada = false,
   refresh_interval = 250,
   sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
-  excluded_filetypes = {},
-  excluded_buftypes = {},
+  excluded_filetypes = {
+    "NvimTree",
+  },
+  excluded_buftypes = {
+    "nofile",
+    "prompt",
+    "popup",
+  },
   bookmark_0 = {
     sign = "⚑",
     virt_text = "Bookmark",
