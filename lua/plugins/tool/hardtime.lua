709627b1830fe99b9fc2b900eaf543d4ee2a3e24
Wei Tan <tanwei0404@163.com>
2025-10-05 12:35:58 +0800

    config: Configure for hardtime.nvim

--- lua/plugins/tool/hardtime.lua
+++ lua/plugins/tool/hardtime.lua
@@ -2,14 +2,19 @@ local M = {
   "m4xshen/hardtime.nvim",
   dependencies = {
     "MunifTanjim/nui.nvim",
-    "nvim-lua/plenary.nvim",
   },
-  cmd = "Hardtime",
+  cmd = "Hardtime enable",
 }
 
 M.opts = {
-  enables = false,
-  disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil" },
+  disabled_filetypes = {
+    "qf",
+    "netrw",
+    "NvimTree",
+    "lazy",
+    "mason",
+    "oil",
+  },
 }
 
 return M
