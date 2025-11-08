df959a507519ddc8419497696b8e9774c0017510
Wei Tan <tanwei0404@163.com>
2025-11-08 18:33:51 +0800

    colorscheme: Add tairiki.nvim

---
+++ lua/plugins/colorscheme/tairiki.lua
@@ -0,0 +1,12 @@
+local M = {
+  "deparr/tairiki.nvim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "tairiki")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
