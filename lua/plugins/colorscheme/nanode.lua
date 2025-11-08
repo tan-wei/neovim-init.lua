c0debed8bb76991b0b63de6c6140e8b2135542c6
Wei Tan <tanwei0404@163.com>
2025-11-08 18:38:06 +0800

    colorscheme: Add nanode.nvim

---
+++ lua/plugins/colorscheme/nanode.lua
@@ -0,0 +1,14 @@
+local M = {
+  "KijitoraFinch/nanode.nvim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "nanode")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+M.config = true
+
+return M
