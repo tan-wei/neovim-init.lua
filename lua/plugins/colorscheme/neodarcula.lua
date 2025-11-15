28db566935603b27de167b39912693eadbe0655a
Wei Tan <tanwei0404@163.com>
2025-11-15 22:37:27 +0800

    fix: Fix typo

--- lua/plugins/colorscheme/neodarcula.lua
+++ lua/plugins/colorscheme/neodarcula.lua
@@ -5,7 +5,7 @@ local M = {
 
 M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
-  table.insert(available_colorschemes, "neofusion")
+  table.insert(available_colorschemes, "neodarcula")
   vim.g.available_colorschemes = available_colorschemes
 end
 
