58511bba7827ea64e30df36de92c4f70feb98058
Wei Tan <tanwei0404@163.com>
2025-12-13 17:04:25 +0800

    plugin: Add nvim-tree-preview.lua

--- lua/plugins/filesystem/nvim-tree.lua
+++ lua/plugins/filesystem/nvim-tree.lua
@@ -2,6 +2,11 @@ local M = {
   "nvim-tree/nvim-tree.lua",
   dependencies = {
     "nvim-tree/nvim-web-devicons",
+    "b0o/nvim-tree-preview.lua",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "3rd/image.nvim",
+    },
   },
   event = "VimEnter",
 }
@@ -124,6 +129,38 @@ M.config = function()
     git = {
       timeout = 2000,
     },
+    on_attach = function(bufnr)
+      local api = require "nvim-tree.api"
+
+      api.config.mappings.default_on_attach(bufnr)
+
+      local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+
+      local preview = require "nvim-tree-preview"
+
+      vim.keymap.set("n", "P", preview.watch, opts "Preview (Watch)")
+      vim.keymap.set("n", "<Esc>", preview.unwatch, opts "Close Preview/Unwatch")
+      vim.keymap.set("n", "<C-f>", function()
+        return preview.scroll(4)
+      end, opts "Scroll Down")
+      vim.keymap.set("n", "<C-b>", function()
+        return preview.scroll(-4)
+      end, opts "Scroll Up")
+
+      -- Smart tab behavior: Only preview files, expand/collapse directories (recommended)
+      vim.keymap.set("n", "<Tab>", function()
+        local ok, node = pcall(api.tree.get_node_under_cursor)
+        if ok and node then
+          if node.type == "directory" then
+            api.node.open.edit()
+          else
+            preview.node(node, { toggle_focus = true })
+          end
+        end
+      end, opts "Preview")
+    end,
   }
 end
 
