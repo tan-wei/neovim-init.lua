8e63acc4a7fc18c9ac62790f5a0d54664c96cb41
Wei Tan <tanwei0404@163.com>
2025-12-13 12:38:37 +0800

    colorscheme: Add NeoCyberVim

---
+++ lua/plugins/colorscheme/NeoCyberVim.lua
@@ -0,0 +1,14 @@
+local M = {
+  "DonJulve/NeoCyberVim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "NeoCyberVim")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+M.config = true
+
+return M
