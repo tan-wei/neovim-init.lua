c4df79fdee7bdacbdcf0859d66e8c94e07144a7e
Wei Tan <tanwei0404@163.com>
2025-12-13 16:25:36 +0800

    colorscheme: Add nightwolf.nvim

--- lua/plugins/colorscheme/nightwolf.lua
+++ lua/plugins/colorscheme/nightwolf.lua
@@ -5,7 +5,10 @@ local M = {
 
 M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
-  table.insert(available_colorschemes, "nightwolf")
+  table.insert(available_colorschemes, "nightwolf-black")
+  table.insert(available_colorschemes, "nightwolf-gray")
+  table.insert(available_colorschemes, "nightwolf-dark-gray")
+  table.insert(available_colorschemes, "nightwolf-dark-blue")
   vim.g.available_colorschemes = available_colorschemes
 end
 
