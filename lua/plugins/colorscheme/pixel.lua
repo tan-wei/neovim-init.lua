cdec22c5e3e54e20552fe496fc3129a671470f7f
Wei Tan <tanwei0404@163.com>
2025-12-22 19:49:33 +0800

    colorscheme: Only enable pixel on CUI

--- lua/plugins/colorscheme/pixel.lua
+++ lua/plugins/colorscheme/pixel.lua
@@ -4,9 +4,11 @@ local M = {
 }
 
 M.init = function()
-  local available_colorschemes = vim.g.available_colorschemes or {}
-  table.insert(available_colorschemes, "pixel")
-  vim.g.available_colorschemes = available_colorschemes
+  if require("util.client").is_cui_client() then
+    local available_colorschemes = vim.g.available_colorschemes or {}
+    table.insert(available_colorschemes, "pixel")
+    vim.g.available_colorschemes = available_colorschemes
+  end
 end
 
 return M
