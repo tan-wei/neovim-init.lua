e4e788ccd6542b6ed76c0e96d40b28e1c24e64e6
Wei Tan <tanwei0404@163.com>
2025-12-26 09:19:11 +0800

    colorscheme: Add gruvdark.nvim

---
+++ lua/plugins/colorscheme/gruvdark.lua
@@ -0,0 +1,14 @@
+local M = {
+  "darianmorat/gruvdark.nvim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "gruvdark")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+M.config = true
+
+return M
