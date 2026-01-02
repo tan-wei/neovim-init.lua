3d7b06e388ec5cf0a6fa9bf52376491f34516cbf
Wei Tan <tanwei0404@163.com>
2026-01-02 22:04:15 +0800

    colorscheme: Add silkcircuit-nvim

---
+++ lua/plugins/colorscheme/silkcircuit-nvim.lua
@@ -0,0 +1,14 @@
+local M = {
+  "hyperb1iss/silkcircuit-nvim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "silkcircuit")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+M.config = true
+
+return M
