65225fc326363f747d2d2fd0dab56309009e9cfc
Wei Tan <tanwei0404@163.com>
2026-01-11 17:18:44 +0800

    plugin: Add codediff.nvim

---
+++ lua/plugins/git/codediff.lua
@@ -0,0 +1,14 @@
+local M = {
+  "esmuellert/codediff.nvim",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  cmd = {
+    "CodeDiff",
+  },
+}
+
+-- TODO: This plugin should write more configurations
+M.config = true
+
+return M
