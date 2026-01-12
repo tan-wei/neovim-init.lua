668a257628c177fc1c6cde6c5568c1f15f5eb4a9
Wei Tan <tanwei0404@163.com>
2026-01-12 21:53:47 +0800

    colorscheme: Add colorscheme variants in boo-colorscheme-nvim

--- lua/plugins/colorscheme/boo-colorscheme-nvim.lua
+++ lua/plugins/colorscheme/boo-colorscheme-nvim.lua
@@ -6,6 +6,10 @@ local M = {
 M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
   table.insert(available_colorschemes, "boo")
+  table.insert(available_colorschemes, "sunset_cloud")
+  table.insert(available_colorschemes, "radioactive_waste")
+  table.insert(available_colorschemes, "forest_stream")
+  table.insert(available_colorschemes, "crimson_moonlight")
   vim.g.available_colorschemes = available_colorschemes
 end
 
