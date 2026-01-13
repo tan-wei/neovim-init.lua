47940b13d5e2a3d3f12318bfb11ed132fb104ab4
Wei Tan <tanwei0404@163.com>
2026-01-13 23:20:34 +0800

    colorscheme: Add yoda.nvim

---
+++ lua/plugins/colorscheme/yoda.lua
@@ -0,0 +1,12 @@
+local M = {
+  "kuri-sun/yoda.nvim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "yoda")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
