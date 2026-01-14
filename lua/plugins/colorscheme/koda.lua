6b7d887770ed10021e2a406878d25fb5f8d143e3
Wei Tan <tanwei0404@163.com>
2026-01-14 20:50:37 +0800

    colorscheme: Add koda.nvim

---
+++ lua/plugins/colorscheme/koda.lua
@@ -0,0 +1,12 @@
+local M = {
+  "oskarnurm/koda.nvim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "koda")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
