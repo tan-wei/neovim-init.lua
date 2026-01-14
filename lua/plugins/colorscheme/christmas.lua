0edfe21c1e59be14bfc65cfb853eb0117a2b4053
Wei Tan <tanwei0404@163.com>
2026-01-14 22:19:07 +0800

    colorscheme: Add christmas.nvim

---
+++ lua/plugins/colorscheme/christmas.lua
@@ -0,0 +1,12 @@
+local M = {
+  "chaserensberger/christmas.nvim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "christmas")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
