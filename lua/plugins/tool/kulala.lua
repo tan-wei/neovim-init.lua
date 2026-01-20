31d5a8b7ead392ee073bbfdad6ab41cdb6c2ea58
Wei Tan <tanwei0404@163.com>
2026-01-20 21:54:51 +0800

    plugin: Add kulala.nvim

---
+++ lua/plugins/tool/kulala.lua
@@ -0,0 +1,12 @@
+local M = {
+  "mistweaverco/kulala.nvim",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  ft = { "http", "rest" },
+}
+
+-- TODO: This plugin should write more configurations, keymaps should be added
+M.config = true
+
+return M
