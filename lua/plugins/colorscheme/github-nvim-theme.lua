e68e4a0ffb1e41996ef2e74cf32d3141235c7f9f
Wei Tan <tanwei0404@163.com>
2026-01-21 22:14:31 +0800

    colorscheme: Add github_dark_default variant

--- lua/plugins/colorscheme/github-nvim-theme.lua
+++ lua/plugins/colorscheme/github-nvim-theme.lua
@@ -7,6 +7,7 @@ M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
   table.insert(available_colorschemes, "github_dark")
   table.insert(available_colorschemes, "github_dark_dimmed")
+  table.insert(available_colorschemes, "github_dark_default")
   table.insert(available_colorschemes, "github_dark_high_contrast")
   table.insert(available_colorschemes, "github_dark_colorblind")
   table.insert(available_colorschemes, "github_dark_tritanopia")
