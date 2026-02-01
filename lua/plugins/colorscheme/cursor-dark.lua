262f9d42d79bcbc0da3614cbc193d1d157d7ab03
Wei Tan <tanwei0404@163.com>
2026-02-01 22:29:46 +0800

    colorscheme: Add cursor-dark.nvim

---
+++ lua/plugins/colorscheme/cursor-dark.lua
@@ -0,0 +1,12 @@
+local M = {
+  "bergholmm/cursor-dark.nvim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "cursor-dark")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
