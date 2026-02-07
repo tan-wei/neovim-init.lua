70cfb2b7dfb318ad8191c0bc436669181ec3e6c5
Wei Tan <tanwei0404@163.com>
2026-02-07 14:01:58 +0800

    colorscheme: Add naysayer.nvim

---
+++ lua/plugins/colorscheme/naysayer.lua
@@ -0,0 +1,12 @@
+local M = {
+  "RostislavArts/naysayer.nvim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "naysayer")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
