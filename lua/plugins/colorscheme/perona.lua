f50fcfec407c8781682152cbc3d2e8c3ecd72f08
Wei Tan <tanwei0404@163.com>
2026-02-07 19:04:43 +0800

    colorscheme: Add perona.nvim

---
+++ lua/plugins/colorscheme/perona.lua
@@ -0,0 +1,15 @@
+local M = {
+  "sailorfe/perona.nvim",
+  dependencies = { 
+    "rktjmp/lush.nvim" 
+  },
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "perona")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
