5e44f618077cc1dfcc18271bbb157796a7318e64
Wei Tan <tanwei0404@163.com>
2026-02-08 10:20:10 +0800

    colorscheme: Add lux.nvim

--- lua/plugins/colorscheme/lux.lua
+++ lua/plugins/colorscheme/lux.lua
@@ -7,8 +7,8 @@ M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
   table.insert(available_colorschemes, "lux-vesper")
   table.insert(available_colorschemes, "lux-umbra")
-  table.insert(available_colorschemes, "lux-aurora")
   vim.g.available_colorschemes = available_colorschemes
 end
 
 return M
+ß
