7a43f01947bef1f182e760dde12d8646ce2e820b
Wei Tan <tanwei0404@163.com>
2026-02-08 22:03:48 +0800

    colorscheme: Add variants in noctis-nvim

--- lua/plugins/colorscheme/noctis-nvim.lua
+++ lua/plugins/colorscheme/noctis-nvim.lua
@@ -6,6 +6,14 @@ local M = {
 M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
   table.insert(available_colorschemes, "noctis")
+  table.insert(available_colorschemes, "noctis-vscode")
+  table.insert(available_colorschemes, "noctis-azureus")
+  table.insert(available_colorschemes, "noctis-bordo")
+  table.insert(available_colorschemes, "noctis-obscuro")
+  table.insert(available_colorschemes, "noctis-sereno")
+  table.insert(available_colorschemes, "noctis-uva")
+  table.insert(available_colorschemes, "noctis-viola")
+  table.insert(available_colorschemes, "noctis-minimus")
   vim.g.available_colorschemes = available_colorschemes
 end
 
