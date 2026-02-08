d9d7dbe9076c32669d3143357193b3978d2d2326
Wei Tan <tanwei0404@163.com>
2026-02-08 21:49:34 +0800

    colorscheme: Add variants in zenbones.nvim

--- lua/plugins/colorscheme/zenbones.lua
+++ lua/plugins/colorscheme/zenbones.lua
@@ -6,6 +6,19 @@ local M = {
 M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
   table.insert(available_colorschemes, "zenbones")
+  table.insert(available_colorschemes, "zenwritten")
+  table.insert(available_colorschemes, "vimbones")
+  table.insert(available_colorschemes, "rosebones")
+  table.insert(available_colorschemes, "forestbones")
+  table.insert(available_colorschemes, "nordbones")
+  table.insert(available_colorschemes, "tokyobones")
+  table.insert(available_colorschemes, "seoulbones")
+  table.insert(available_colorschemes, "duckbones")
+  table.insert(available_colorschemes, "zenburned")
+  table.insert(available_colorschemes, "kanagawabones")
+
+  -- Or use this?
+  -- table.insert(available_colorschemes, "randombones")
   vim.g.available_colorschemes = available_colorschemes
 end
 
