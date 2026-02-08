2e3b6465452cfaac325033067c151ec9e35f2e40
Wei Tan <tanwei0404@163.com>
2026-02-08 08:35:35 +0800

    colorscheme: Add Alienocean.nvim

---
+++ lua/plugins/colorscheme/Alienocean.lua
@@ -0,0 +1,17 @@
+local M = {
+  "Pair-of-dice/Alienocean.nvim",
+  dependencies = {
+		"Pair-of-dice/Alienocean-lualine",
+	},
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "Alienocean")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+M.config = true
+
+return M
