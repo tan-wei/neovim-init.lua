4f005b3896f8cde7feb1b93eff41590bb2210bd8
Wei Tan <tanwei0404@163.com>
2026-02-12 21:27:11 +0800

    colorscheme: Add fieldlights.nvim

---
+++ lua/plugins/colorscheme/fieldlights.lua
@@ -0,0 +1,14 @@
+local M = {
+  "waytoopurple/fieldlights.nvim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "fieldlights")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+M.config = true
+
+return M
