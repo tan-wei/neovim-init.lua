966d1414900855a777870b02c86c9657912dc138
Wei Tan <tanwei0404@163.com>
2026-03-24 20:28:21 +0800

    colorscheme: Remove vimbones light variant

--- lua/plugins/colorscheme/zenbones.lua
+++ lua/plugins/colorscheme/zenbones.lua
@@ -7,7 +7,6 @@ M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
   table.insert(available_colorschemes, "zenbones")
   table.insert(available_colorschemes, "zenwritten")
-  table.insert(available_colorschemes, "vimbones")
   table.insert(available_colorschemes, "rosebones")
   table.insert(available_colorschemes, "forestbones")
   table.insert(available_colorschemes, "nordbones")
