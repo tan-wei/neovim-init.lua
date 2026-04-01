74a24c33668f1ce1f941f3c38ec1ee3bd48efa37
Wei Tan <tanwei0404@163.com>
2026-04-01 13:51:30 +0800

    colorscheme: Change variants in sequoia.nvim

--- lua/plugins/colorscheme/sequoia.lua
+++ lua/plugins/colorscheme/sequoia.lua
@@ -7,7 +7,9 @@ M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
   table.insert(available_colorschemes, "sequoia")
   table.insert(available_colorschemes, "sequoia-night")
-  table.insert(available_colorschemes, "sequoia-insomnia")
+  table.insert(available_colorschemes, "sequoia-fog")
+  table.insert(available_colorschemes, "sequoia-ember")
+  table.insert(available_colorschemes, "sequoia-moss")
   vim.g.available_colorschemes = available_colorschemes
 end
 
