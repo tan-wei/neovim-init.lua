3b7a76c90f1860ef7e7947342addab994240ed16
Wei Tan <tanwei0404@163.com>
2026-04-08 15:05:12 +0800

    fix: Remove useless comment

--- lua/plugins/ui/modicator.lua
+++ lua/plugins/ui/modicator.lua
@@ -6,7 +6,6 @@ local M = {
   event = "ModeChanged",
 }
 
--- TODO: Now it works well, but if we change the colorschme, it will not work correctly
 M.opts = {
   show_warnings = false,
   highlights = {
