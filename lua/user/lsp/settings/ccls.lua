2d3fcf98a274e6baee6ca9280a2acef751021f37
Wei Tan <tanwei0404@163.com>
2026-04-20 11:15:16 +0800

    lsp: Disable some useless errors

--- lua/user/lsp/settings/ccls.lua
+++ lua/user/lsp/settings/ccls.lua
@@ -40,11 +40,33 @@ return {
     sc.hoverProvider = false
     sc.codeActionProvider = false
   end,
-  -- Suppress diagnostics and signature (mirrors ccls.nvim's nil_handlers)
-  handlers = {
-    ["textDocument/publishDiagnostics"] = noopfn,
-    ["textDocument/signatureHelp"] = noopfn,
-  },
+  -- Suppress diagnostics, signature, and "not indexed" response errors from ccls.
+  -- Wrap common request handlers to silently ignore -32600 ("not indexed") errors.
+  handlers = (function()
+    local function wrap_handler(method)
+      return function(err, result, ctx, config)
+        if err and err.code == -32600 then
+          return -- silently ignore "not indexed"
+        end
+        -- Fall through to default handler
+        return vim.lsp.handlers[method](err, result, ctx, config)
+      end
+    end
+    return {
+      ["textDocument/publishDiagnostics"] = noopfn,
+      ["textDocument/signatureHelp"] = noopfn,
+      ["textDocument/definition"] = wrap_handler "textDocument/definition",
+      ["textDocument/references"] = wrap_handler "textDocument/references",
+      ["textDocument/hover"] = wrap_handler "textDocument/hover",
+      ["textDocument/implementation"] = wrap_handler "textDocument/implementation",
+      ["textDocument/typeDefinition"] = wrap_handler "textDocument/typeDefinition",
+      ["textDocument/documentSymbol"] = wrap_handler "textDocument/documentSymbol",
+      ["textDocument/codeAction"] = wrap_handler "textDocument/codeAction",
+      ["textDocument/codeLens"] = wrap_handler "textDocument/codeLens",
+      ["callHierarchy/incomingCalls"] = wrap_handler "callHierarchy/incomingCalls",
+      ["callHierarchy/outgoingCalls"] = wrap_handler "callHierarchy/outgoingCalls",
+    }
+  end)(),
   init_options = {
     index = {
       threads = 2,
