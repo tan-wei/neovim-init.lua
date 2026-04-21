ff4626bed86910eeea22137fb08895ad2eb8a887
Wei Tan <tanwei0404@163.com>
2026-04-21 19:46:48 +0800

    fix: Fix typo

--- lua/plugins/colorscheme/nightingale.lua
+++ lua/plugins/colorscheme/nightingale.lua
@@ -5,7 +5,7 @@ local M = {
 
 M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
-  table.insert(available_colorschemes, "nigtingale")
+  table.insert(available_colorschemes, "nightingale")
   vim.g.available_colorschemes = available_colorschemes
 end
 
