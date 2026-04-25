96034de9878dd8979478ca0a7add7e4f0a0ca025
Wei Tan <tanwei0404@163.com>
2026-04-25 18:16:40 +0800

    colorscheme: Add bearded-arc.nvim

---
+++ lua/plugins/colorscheme/bearded-arc.lua
@@ -0,0 +1,18 @@
+local M = {
+  "kcayme/bearded-arc.nvim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "bearded-arc")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+M.opts = {
+  plugins = {
+    all = true,
+  }
+}
+
+return M
