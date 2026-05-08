3876e01c3890543cee04caee3998839aebd84c3c
Wei Tan <tanwei0404@163.com>
2026-05-08 21:40:30 +0800

    colorscheme: Add Oshen.nvim

---
+++ lua/plugins/colorscheme/Oshen.lua
@@ -0,0 +1,18 @@
+local M = {
+  "54L1M/Oshen.nvim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "Oshen")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+M.config = function()
+  require("oshen").setup {
+    transparent = false,
+  }
+end
+
+return M
