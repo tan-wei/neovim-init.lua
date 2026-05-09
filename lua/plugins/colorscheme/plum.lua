4a160e1e47c4b9d9bf4c732dfc309779b68f54cc
Wei Tan <tanwei0404@163.com>
2026-05-09 10:07:24 +0800

    colorscheme: Add plum.nvim

---
+++ lua/plugins/colorscheme/plum.lua
@@ -0,0 +1,16 @@
+local M = {
+  "takeshid/plum.nvim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "plum")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+M.opts = {
+  variant = "dark",
+}
+
+return M
