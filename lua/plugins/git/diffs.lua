5cad47e34dfb6a5c6908f4fc072e8ee4c4edd584
Wei Tan <tanwei0404@163.com>
2026-05-09 17:01:34 +0800

    plugin: Add diffs.nvim

---
+++ lua/plugins/git/diffs.lua
@@ -0,0 +1,17 @@
+local M = {
+  "barrettruth/diffs.nvim",
+  lazy = false, -- NOTE: diffs.nvim lazy-loads itself
+}
+
+M.init = function()
+  vim.g.diffs = {
+    integrations = {
+      fugitive = true,
+      neogit = true,
+      neojj = false,
+      gitsigns = true,
+    },
+  }
+end
+
+return M
