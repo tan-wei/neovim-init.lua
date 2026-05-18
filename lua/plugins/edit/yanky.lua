643f6d2d025a61c3f481592a6f353257de405470
Wei Tan <tanwei0404@163.com>
2026-05-18 09:32:43 +0800

    config: Configure for yangky.nvim

--- lua/plugins/edit/yanky.lua
+++ lua/plugins/edit/yanky.lua
@@ -1,17 +1,39 @@
 local M = {
   "gbprod/yanky.nvim",
+  lazy = false,
   dependencies = {
     "kkharji/sqlite.lua",
     "nvim-telescope/telescope.nvim",
   },
-  event = "VeryLazy",
+  cmd = {
+    "YankyClearHistory",
+    "YankyRingHistory",
+  },
 }
 
 -- TODO: Integerate with other plugins
 
 M.config = function()
   local yanky = require "yanky"
+  local telescope_mapping = require "yanky.telescope.mapping"
   local yanky_utils = require "yanky.utils"
+  local upstream_get_default_register = yanky_utils.get_default_register
+
+  yanky_utils.get_default_register = function()
+    local register = upstream_get_default_register()
+
+    -- In this setup, native y/p still round-trip through the unnamed register
+    -- even with `clipboard=unnamedplus`, while Yanky's heuristic prefers `+`.
+    -- Keep Yanky aligned with native paste behavior so `yy` followed by `p`
+    -- reuses the most recent yank instead of stale system clipboard content.
+    if register == "+" or register == "*" then
+      return '"'
+    end
+
+    return register
+  end
+
+  local default_register = yanky_utils.get_default_register()
 
   -- Some clipboard providers expose non-text payloads as unreadable registers.
   -- Skip seeding Yanky's history on startup when the default register can't be read.
@@ -28,6 +50,26 @@ M.config = function()
   yanky.setup {
     ring = {
       history_length = 200,
+      storage = "sqlite",
+    },
+    picker = {
+      telescope = {
+        use_default_mappings = false,
+        mappings = {
+          default = telescope_mapping.put "p",
+          i = {
+            ["<C-y>"] = telescope_mapping.put "p",
+            ["<C-e>"] = telescope_mapping.put "P",
+            ["<C-r>"] = telescope_mapping.set_register(default_register),
+          },
+          n = {
+            p = telescope_mapping.put "p",
+            P = telescope_mapping.put "P",
+            d = telescope_mapping.delete(),
+            r = telescope_mapping.set_register(default_register),
+          },
+        },
+      },
     },
     highlight = {
       on_put = false,
