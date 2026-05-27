e23cac19e754d16497f2c9e6f5e6e9e64cb6dba3
Wei Tan <tanwei0404@163.com>
2026-05-27 11:22:46 +0800

    config: Enable democolon.nvim

--- lua/plugins/keymap/democolon.lua
+++ lua/plugins/keymap/democolon.lua
@@ -1,52 +1,54 @@
 ---@type LazyPluginSpec
 local M = {
   "mawkler/demicolon.nvim",
-  enabled = false,
-  event = "VeryLazy",
+  event = "BufReadPost",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    "nvim-neotest/neotest",
+  },
 }
 
 -- TODO: Add custom keymaps for other plugins
 M.opts = {
-  highlight = true,
-}
-
-M.opts = {
-  diagnostic = {
-    float = {},
-  },
   keymaps = {
     horizontal_motions = false,
     diagnostic_motions = true,
-    repeat_motions = false,
+    repeat_motions = "stateful",
     list_motions = true,
     spell_motions = true,
     fold_motions = true,
   },
-  integrations = {
-    gitsigns = {
-      enabled = true,
-      keymaps = {
-        next = "]c",
-        prev = "[c",
-      },
-    },
-    neotest = {
-      enabled = true,
-      keymaps = {
-        test = {
-          next = "]t",
-          prev = "[t",
-        },
-        failed_test = {
-          next = "]T",
-          prev = "[T",
-        },
-      },
-    },
-    vimtex = {
-      enabled = false,
-    },
-  },
 }
+
+M.config = function(_, opts)
+  require("demicolon").setup(opts)
+
+  local jump = require("demicolon.jump")
+
+  local function set_neotest_jump(lhs, forward, jump_opts, desc)
+    vim.keymap.set("n", lhs, function()
+      jump.repeatably_do(function(repeat_opts)
+        local direction = repeat_opts.forward and "next" or "prev"
+        require("neotest").jump[direction](jump_opts)
+      end, { forward = forward })
+    end, { desc = desc })
+  end
+
+  set_neotest_jump("]t", true, nil, "Next test")
+  set_neotest_jump("[t", false, nil, "Previous test")
+  set_neotest_jump("]T", true, { status = "failed" }, "Next failed test")
+  set_neotest_jump("[T", false, { status = "failed" }, "Previous failed test")
+end
+
+-- Custom jumps example (uncomment to use)
+-- local jump = require("demicolon.jump")
+-- vim.keymap.set("n", "<leader>s", function()
+--   jump.repeatably_do(function(opts)
+--     require("flash").jump({
+--       forward = opts.forward,
+--     })
+--   end, { forward = true })
+-- end, { desc = "Flash jump (repeatable)" })
 
 return M
