5255682d79cdbb079a780bf1fe3a5254825b7bbe
Wei Tan <tanwei0404@163.com>
2026-05-27 11:19:01 +0800

    config: Enable coerce

--- lua/plugins/edit/coerce.lua
+++ lua/plugins/edit/coerce.lua
@@ -1,10 +1,29 @@
 ---@type LazyPluginSpec
 local M = {
   "gregorias/coerce.nvim",
-  enabled = false,
+  event = "VeryLazy",
+  dependencies = {
+    "gregorias/coop.nvim",
+  },
 }
 
--- TODO: This plugin should write more configurations, keymaps should be added
-M.config = true
+M.config = function()
+  require("coerce").setup()
+
+  -- Set up keymaps with which-key
+  local ok, wk = pcall(require, "which-key")
+  if ok then
+    local wke = require("coerce.keymaps").which_key_expand
+    wk.add({
+      { "cr", group = "+Coerce word", expand = wke.normal_mode, mode = "n" },
+      { "gcr", group = "+Coerce motion", expand = wke.motion_mode, mode = "n" },
+      { "gcr", group = "+Coerce visual", expand = wke.visual_mode, mode = "x" },
+    })
+  else
+    vim.keymap.set("n", "cr", "<Plug>(coerce-normal)", { desc = "Coerce word" })
+    vim.keymap.set("n", "gcr", "<Plug>(coerce-motion)", { desc = "Coerce motion" })
+    vim.keymap.set("x", "gcr", "<Plug>(coerce-visual)", { desc = "Coerce visual" })
+  end
+end
 
 return M
