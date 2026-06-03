4f76417851fbf70bffd1523802ebb9989f433dd7
Wei Tan <tanwei0404@163.com>
2026-06-03 15:47:08 +0800

    plugin: Use a fork version instead

--- lua/plugins/lsp/nvim-lsp-file-operations.lua
+++ lua/plugins/lsp/nvim-lsp-file-operations.lua
@@ -1,7 +1,8 @@
 ---@type LazyPluginSpec
 local M = {
 
-  "antosha417/nvim-lsp-file-operations",
+  -- "antosha417/nvim-lsp-file-operations",
+  "DrKJeff16/nvim-lsp-file-operations", -- NOTE: Use this fork to solve issues
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-tree.lua",
