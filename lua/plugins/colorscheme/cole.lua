dbba108c46f02e269c73df96c4eefe12638491be
Wei Tan <tanwei0404@163.com>
2026-06-06 20:30:37 +0800

    colorscheme: Add cole.nvim

---
+++ lua/plugins/colorscheme/cole.lua
@@ -0,0 +1,13 @@
+---@type LazyPluginSpec
+local M = {
+  "thekylehuang/cole.nvim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "cole")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
