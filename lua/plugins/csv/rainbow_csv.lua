1adbe39beb9bd9214fccbb76e78aa9149133446f
Wei Tan <tanwei0404@163.com>
2026-07-08 14:54:17 +0800

    config: Remove useless code

--- lua/plugins/csv/rainbow_csv.lua
+++ lua/plugins/csv/rainbow_csv.lua
@@ -5,18 +5,18 @@ local M = {
 }
 
 M.config = function()
-  vim.g.rcsv_colorpairs = {
-    { "red", "red" },
-    { "blue", "blue" },
-    { "green", "green" },
-    { "magenta", "magenta" },
-    { "NONE", "NONE" },
-    { "darkred", "darkred" },
-    { "darkblue", "darkblue" },
-    { "darkgreen", "darkgreen" },
-    { "darkmagenta", "darkmagenta" },
-    { "darkcyan", "darkcyan" },
-  }
+  -- vim.g.rcsv_colorpairs = {
+  --   { "red", "red" },
+  --   { "blue", "blue" },
+  --   { "green", "green" },
+  --   { "magenta", "magenta" },
+  --   { "NONE", "NONE" },
+  --   { "darkred", "darkred" },
+  --   { "darkblue", "darkblue" },
+  --   { "darkgreen", "darkgreen" },
+  --   { "darkmagenta", "darkmagenta" },
+  --   { "darkcyan", "darkcyan" },
+  -- }
 end
 
 return M
