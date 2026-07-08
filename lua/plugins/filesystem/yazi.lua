81fcb22e98bec14f5a0e0626eb101f55f350a808
Wei Tan <tanwei0404@163.com>
2026-07-08 14:54:41 +0800

    config: Configure for yazi.nvim

--- lua/plugins/filesystem/yazi.lua
+++ lua/plugins/filesystem/yazi.lua
@@ -7,7 +7,46 @@ local M = {
   cmd = { "Yazi" },
 }
 
--- TODO: This plugin should write more configurations
-M.config = true
+M.opts = {
+  yazi_floating_window_winblend = 10,
+  yazi_floating_window_border = "rounded",
+  floating_window_scaling_factor = 0.9,
+  integrations = {
+    grep_in_directory = function(directory)
+      require("grug-far").open {
+        prefills = {
+          paths = directory,
+        },
+      }
+    end,
+    grep_under_cursor = function()
+      local word = vim.fn.expand "<cword>"
+      require("grug-far").open {
+        prefills = {
+          search = word,
+        },
+      }
+    end,
+  },
+  keymaps = {
+    show_help = "<F1>",
+    grep_under_cursor = "<a-g>",
+    open_file_in_vertical_split = "<c-v>",
+    open_file_in_horizontal_split = "<c-x>",
+    open_file_in_tab = "<c-t>",
+    grep_in_directory = "<c-s>",
+    replace_in_directory = "<c-g>",
+    cycle_open_buffers = "<tab>",
+    copy_relative_path_to_selected_files = "<c-y>",
+    send_to_quickfix_list = "<c-q>",
+    change_working_directory = "<c-\\>",
+    open_and_pick_window = "<c-o>",
+  },
+
+  open_file_function = function(entry)
+    vim.cmd "Yazi"
+    vim.cmd("edit " .. entry.path)
+  end,
+}
 
 return M
