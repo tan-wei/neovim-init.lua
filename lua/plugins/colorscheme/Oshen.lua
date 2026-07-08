d659efe21f089a37af732db9993ee2f5cd26be76
Wei Tan <tanwei0404@163.com>
2026-07-08 11:36:30 +0800

    colorscheme: Fix colorscheme name

--- lua/plugins/colorscheme/Oshen.lua
+++ lua/plugins/colorscheme/Oshen.lua
@@ -6,7 +6,7 @@ local M = {
 
 M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
-  table.insert(available_colorschemes, "Oshen")
+  table.insert(available_colorschemes, "oshen-night")
   vim.g.available_colorschemes = available_colorschemes
 end
 
