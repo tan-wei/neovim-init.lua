9881f6fd3652bcbfe4dd1d720e1a6f8fba9829f2
Wei Tan <tanwei0404@163.com>
2026-07-08 14:54:53 +0800

    config: Configure for buffer_manager.nvim

--- lua/plugins/ui/buffer_manager.lua
+++ lua/plugins/ui/buffer_manager.lua
@@ -7,7 +7,35 @@ local M = {
   event = { "BufEnter" },
 }
 
--- TODO: This plugin should write more configurations
-M.config = true
+M.opts = {
+  line_keys = "1234567890",
+  select_menu_item_commands = {
+    edit = {
+      key = "<CR>",
+      command = "edit",
+    },
+  },
+  focus_alternate_buffer = false,
+  width = nil,
+  height = nil,
+  short_file_names = false,
+  show_depth = true,
+  short_term_names = false,
+  show_cols = "both", -- "kbs", "both"
+  loop_nav = true,
+  highlight = "",
+  win_extra_options = {},
+  borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+  format_function = nil,
+  order_buffers = "lastused", -- "filename", "bufnr", "lastused", "fullpath"
+  show_indicators = "before",
+  toggle_key_bindings = { "q", "<ESC>" },
+  use_shortcuts = false,
+  win_position = { h = 0.5, v = 0.5 },
+  quick_kbs = {
+    enabled = false,
+    kb = nil,
+  },
+}
 
 return M
