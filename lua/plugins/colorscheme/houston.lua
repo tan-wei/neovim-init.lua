da3d0f1ee8e06a571af2df7453549cbb123f3696
Wei Tan <tanwei0404@163.com>
2026-07-11 12:20:41 +0800

    colorscheme: Add houston.nvim

---
+++ lua/plugins/colorscheme/houston.lua
@@ -0,0 +1,15 @@
+---@type LazyPluginSpec
+local M = {
+  "devbydaniel/houston.nvim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "houston")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+M.config = true
+
+return M
