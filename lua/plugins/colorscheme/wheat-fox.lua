69ebb9e68cbdf90babb5453454bff844950f22b5
Wei Tan <tanwei0404@163.com>
2026-07-11 12:17:15 +0800

    colorscheme: Add wheat-fox.nvim

---
+++ lua/plugins/colorscheme/wheat-fox.lua
@@ -0,0 +1,15 @@
+---@type LazyPluginSpec
+local M = {
+  "saran13raj/wheat-fox.nvim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "wheat-fox")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+M.config = true
+
+return M
