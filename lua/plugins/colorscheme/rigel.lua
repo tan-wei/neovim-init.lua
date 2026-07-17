a3a65a0a8846fd8d40a9db4e8f4bfa227bc60ad7
Wei Tan <tanwei0404@163.com>
2026-07-17 17:42:04 +0800

    colorscheme: Add rigel.nvim

---
+++ lua/plugins/colorscheme/rigel.lua
@@ -0,0 +1,13 @@
+---@type LazyPluginSpec
+local M = {
+  "tan-wei/rigel.nvim",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "rigel")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+return M
