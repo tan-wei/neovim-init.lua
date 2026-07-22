47d010effc4f4e9d8df9eb4f8c1294f33b922622
Wei Tan <tanwei0404@163.com>
2026-07-22 15:33:00 +0800

    fix: Fix typo

--- lua/plugins/colorscheme/helix-nvim.lua
+++ lua/plugins/colorscheme/helix-nvim.lua
@@ -6,7 +6,7 @@ local M = {
 
 M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
-  table.insert(available_colorschemes, "hardhacker")
+  table.insert(available_colorschemes, "helix")
   vim.g.available_colorschemes = available_colorschemes
 end
 
