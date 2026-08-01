4fa0fa34efb31a01a7d6d7bf98403f9b04ea0cdc
Wei Tan <tanwei0404@163.com>
2026-08-01 12:19:01 +0800

    colorscheme: Add cendre

---
+++ lua/plugins/colorscheme/cendre.lua
@@ -0,0 +1,20 @@
+---@type LazyPluginSpec
+local M = {
+  "Aejkatappaja/cendre",
+  lazy = true,
+}
+
+M.init = function()
+  local available_colorschemes = vim.g.available_colorschemes or {}
+  table.insert(available_colorschemes, "cendre")
+  vim.g.available_colorschemes = available_colorschemes
+end
+
+M.config = function()
+  require("cendre").setup {
+    background = "hard", -- "hard" | "medium" | "soft"
+    italic = true,
+  }
+end
+
+return M
