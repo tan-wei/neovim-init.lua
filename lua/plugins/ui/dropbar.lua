c806c38a5fd7c79d12a3efa96b6add65b4bb41c3
Wei Tan <tanwei0404@163.com>
2026-09-21 16:14:45 +0800

    config: Display terminal name in dropbar.nvim

--- lua/plugins/ui/dropbar.lua
+++ lua/plugins/ui/dropbar.lua
@@ -7,6 +7,21 @@ local M = {
   event = "VeryLazy",
 }
 
-M.config = true
+M.opts = {
+  sources = {
+    terminal = {
+      show_current = true,
+      name = function(buf)
+        local name = vim.api.nvim_buf_get_name(buf)
+        local term = select(2, require("toggleterm.terminal").indentify(name))
+        if term then
+          return term.display_name or term.name
+        else
+          return name
+        end
+      end,
+    },
+  },
+}
 
 return M
