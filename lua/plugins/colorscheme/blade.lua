809cc5b702011e39dca6fd003da23ec926d896b2
Wei Tan <tanwei0404@163.com>
2026-09-25 21:56:59 +0800

    config: Update url due to upstream renamed

--- lua/plugins/colorscheme/blade.lua
+++ lua/plugins/colorscheme/blade.lua
@@ -1,12 +1,12 @@
 ---@type LazyPluginSpec
 local M = {
-  "sirzif/tzfn.nvim",
+  "sirzif/blade.nvim",
   lazy = true,
 }
 
 M.init = function()
   local available_colorschemes = vim.g.available_colorschemes or {}
-  table.insert(available_colorschemes, "tzfn")
+  table.insert(available_colorschemes, "blade")
   vim.g.available_colorschemes = available_colorschemes
 end
 
